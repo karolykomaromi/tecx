3d82d8807379380a86ca74420b29c844306fdf6d
Damien80 <Damien80@localhost>
2010-07-05 15:29:16 +0000

    added a first sample for an ambient context and a few pieces for ddd

---
+++ src/TecX/TecX.Common.Test/ExceptionExtensionFixture.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using TecX.Common.Extensions.Error;
+
+namespace TecX.Common.Test
+{
+    [TestClass]
+    public class ExceptionExtensionFixture
+    {
+        [TestMethod]
+        public void CanAddSingleInfo()
+        {
+            try
+            {
+                throw new Exception().WithAdditionalInfo("single info", 42);
+            }
+            catch (Exception ex)
+            {
+                Assert.AreEqual(1, ex.Data.Count);
+                Assert.AreEqual(42, ex.Data["single info"]);
+            }
+        }
+
+        [TestMethod]
+        public void CanAddMoreInfo()
+        {
+            try
+            {
+                throw new Exception().WithAdditionalInfos(new Dictionary<object, object>
+                                                              {
+                                                                  { "first info", 42 }, 
+                                                                  { "scnd info", 666 }
+                                                              });
+            }
+            catch (Exception ex)
+            {
+                Assert.AreEqual(2, ex.Data.Count);
+                Assert.AreEqual(42, ex.Data["first info"]);
+                Assert.AreEqual(666, ex.Data["scnd info"]);
+            }
+        }
+    }
+}
