f8372ce0deb2bc4c176ca9c073bf28c0ba3d48af
Damien80 <Damien80@localhost>
2010-07-25 10:32:56 +0000

    added TestTools and fixtures

---
+++ src/TecX/TecX.TestTools/NotifyChangedExpectation.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.ComponentModel;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using TecX.Common;
+
+namespace TecX.TestTools
+{
+    public class NotifyChangedExpectation<T> where T : INotifyPropertyChanged
+    {
+        private readonly T _owner;
+        private readonly string _propertyName;
+        private readonly bool _eventExpected;
+
+        public NotifyChangedExpectation(T owner, string propertyName, bool eventExpected)
+        {
+            Guard.AssertNotNull(owner, "owner");
+            Guard.AssertNotEmpty(propertyName, "propertyName");
+
+            _owner = owner;
+            _propertyName = propertyName;
+            _eventExpected = eventExpected;
+        }
+
+        public void When(Action<T> action)
+        {
+            Guard.AssertNotNull(action, "action");
+
+            bool eventWasRaised = false;
+
+            _owner.PropertyChanged += (sender, e) =>
+                                          {
+                                              if (e.PropertyName == _propertyName)
+                                              {
+                                                  eventWasRaised = true;
+                                              }
+                                          };
+
+            action(_owner);
+            Assert.AreEqual(_eventExpected, eventWasRaised, "PropertyChanged on {0}",
+                _propertyName);
+        }
+    }
+}
---
+++ src/TecX/TecX.TestTools/NotifyOnCompleted.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using TecX.Common;
+
+namespace TecX.TestTools
+{
+    public static class NotifyOnCompleted
+    {
+        public static NotifyCompletedExpectation<T> AfterCompleting<T>(this T owner, Action<T> action)
+        {
+            Guard.AssertNotNull(owner, "owner");
+            Guard.AssertNotNull(action, "action");
+
+            return new NotifyCompletedExpectation<T>(owner, action);
+        }
+    }
+}
