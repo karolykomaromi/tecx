d2bb9a4cdc62d804b9bc870f0f02879c3c0a578c
Damien80 <Damien80@localhost>
2010-07-31 09:19:03 +0000

    extension methods for combining linq expressions/predicates

---
+++ src/TecX/TecX.Common.Test/EntityPredicateExtensionsFixture.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TecX.Common.Test
+{
+    [TestClass]
+    public class EntityPredicateExtensionsFixture
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
