41de2df5128530a093c760b0067902289be54398
Damien80 <Damien80@localhost>
2010-08-03 19:50:58 +0000

    tests

--- src/TecX/TecX.Agile.Test/AgileBuilderFixture.cs
+++ src/TecX/TecX.Agile.Test/AgileBuilderFixture.cs
@@ -4,6 +4,11 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
+using Ploeh.AutoFixture;
+
+using TecX.Agile.Builder;
+using TecX.TestTools;
+
 namespace TecX.Agile.Test
 {
     /// <summary>
@@ -47,9 +52,75 @@ namespace TecX.Agile.Test
         [TestMethod]
         public void CanBuildStoryCard()
         {
-            //
-            // TODO: Add test logic here
-            //
+            Guid id = Guid.NewGuid();
+
+            StoryCardBuilder builder = New.StoryCard()
+                .WithActualEffort(1.2m)
+                .WithBestCaseEstimate(2.3m)
+                .WithColor(new byte[] { 1, 2, 3, 4 })
+                .WithCurrentSideUp(StoryCardSides.RichTestSide)
+                .WithDescription("desc")
+                .WithDescriptionHandwritingImage(new byte[] { 2, 3, 4, 5, 6, 7, 8, 9 })
+                .WithHeight(3.4)
+                .WithId(id)
+                .WithMostLikelyEstimate(4.5m)
+                .WithName("name")
+                .WithPriority(Priority.Immediate)
+                .WithRotationAngle(5.6)
+                .WithStatus(Status.Completed)
+                .WithTaskOwner("John Wayne")
+                .WithWidth(6.7)
+                .WithWorstCaseEstimate(7.8m)
+                .WithX(8.9)
+                .WithY(9.1);
+
+            StoryCard storyCard = builder.Build();
+
+            Assert.AreEqual(1.2m, storyCard.Tracking.ActualEffort);
+            Assert.AreEqual(2.3m, storyCard.Tracking.BestCaseEstimate);
+            Assert.IsTrue(new byte[] { 1, 2, 3, 4 }.SequenceEqual(storyCard.View.Color));
+            Assert.AreEqual(StoryCardSides.RichTestSide, storyCard.CurrentSideUp);
+            Assert.AreEqual("desc", storyCard.Description);
+            Assert.IsTrue(new byte[] { 2, 3, 4, 5, 6, 7, 8, 9 }.SequenceEqual(storyCard.DescriptionHandwritingImage));
+            Assert.AreEqual(3.4, storyCard.View.Height);
+            Assert.AreEqual(id, storyCard.Id);
+            Assert.AreEqual(4.5m, storyCard.Tracking.MostLikelyEstimate);
+            Assert.AreEqual("name", storyCard.Name);
+            Assert.AreEqual(Priority.Immediate, storyCard.Tracking.Priority);
+            Assert.AreEqual(5.6, storyCard.View.RotationAngle);
+            Assert.AreEqual(Status.Completed, storyCard.Tracking.Status);
+            Assert.AreEqual("John Wayne", storyCard.TaskOwner);
+            Assert.AreEqual(6.7, storyCard.View.Width);
+            Assert.AreEqual(7.8m, storyCard.Tracking.WorstCaseEstimate);
+            Assert.AreEqual(8.9, storyCard.View.X);
+            Assert.AreEqual(9.1, storyCard.View.Y);
+
+            //test implicit conversion
+            StoryCard storyCard2 = builder;
+
+            Assert.AreEqual(storyCard, storyCard2);
+
+            Assert.AreNotSame(storyCard, storyCard2);
+
+            storyCard2 = storyCard2.BuildUp().WithTaskOwner("Prinz Eisenherz");
+
+            Assert.AreNotEqual(storyCard, storyCard2);
+        }
+
+        [TestMethod]
+        public void CanUseAutoFixture()
+        {
+            var fixture = new Fixture
+                              {
+                                  Resolver = FixtureResolver.ResolveEnum
+                              };
+
+            StoryCard storyCard = fixture.CreateAnonymous<StoryCard>();
+            storyCard.View.Color = fixture.CreateAnonymous<byte[]>();
+
+            //fixture.Build<StoryCard>().With()
+
+            Assert.AreNotEqual(0, storyCard.View.Height);
         }
     }
 }
