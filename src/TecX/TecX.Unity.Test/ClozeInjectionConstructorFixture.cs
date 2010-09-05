d64c06df219a7221cd932ff2301c2cd988fc88d3
Damien80 <Damien80@localhost>
2010-09-05 19:40:56 +0000

    fine tuning injectionconstructor

---
+++ src/TecX/TecX.Unity.Test/ClozeInjectionConstructorFixture.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+using Microsoft.Practices.Unity;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using TecX.Unity.Test.TestObjects;
+
+namespace TecX.Unity.Test
+{
+    [TestClass]
+    public class ClozeInjectionConstructorFixture
+    {
+        private static class Constants
+        {
+            /// <summary>blablub</summary>
+            public const string ConnectionStringValue = "blablub";
+        }
+
+        [TestMethod]
+        public void WhenSpecifyingOneCtorArgUnityWillResolveTheOthers()
+        {
+            IUnityContainer container = new UnityContainer();
+
+            container.RegisterType<ILogger, TestLogger>();
+
+            container.RegisterType<ICtorTest, CtorTest>(
+                new ClozeInjectionConstructur("connectionString", Constants.ConnectionStringValue));
+
+            ICtorTest sut = container.Resolve<ICtorTest>();
+            Assert.IsNotNull(sut);
+        }
+
+        [TestMethod]
+        public void WhenSpecifyingCtorArgWithAmbiguousNameCtorIsMatchedByType()
+        {
+            IUnityContainer container = new UnityContainer();
+
+            container.RegisterType<ILogger, TestLogger>();
+
+            container.RegisterType<ICtorTest, CtorTest>(
+                new ClozeInjectionConstructur(new Dictionary<string, object>
+                                                  {
+                                                      {"connectionString", Constants.ConnectionStringValue},
+                                                      {"anotherParam", "I'm a string"}
+                                                  }));
+
+            ICtorTest sut = container.Resolve<ICtorTest>();
+            Assert.IsNotNull(sut);
+        }
+
+        [TestMethod]
+        public void WhenFilterByCtorTakesAllArgsFindsExpectedCtors()
+        {
+            ParameterMatcher matcher = new ParameterMatcher(
+                new Dictionary<string, object>
+                    {
+                        {"connectionString", Constants.ConnectionStringValue}
+                    });
+
+            Predicate<ConstructorInfo> filter = matcher.FilterByCtorTakesAllArgs;
+
+            IEnumerable<Predicate<ConstructorInfo>> filters = new[] {filter};
+
+            var ctors = typeof (CtorTest).GetConstructors();
+
+            var result = ctors.Where(ctor => !filters.Any(f => f(ctor)));
+
+            //must find Ctortest(string,ILogger), CtorTest(string,ILogger,string)
+            //and CtorTest(string,ILogger,int)
+            Assert.AreEqual(3, result.Count());
+        }
+
+        [TestMethod]
+        public void WhenFilterByNonSatisfiedPrimitiveArgsFindsExpectedCtors()
+        {
+            ParameterMatcher matcher = new ParameterMatcher(
+                new Dictionary<string, object>
+                    {
+                        {"connectionString", Constants.ConnectionStringValue}
+                    });
+
+            Predicate<ConstructorInfo> filter = matcher.FilterByNonSatisfiedPrimitiveArgs;
+
+            IEnumerable<Predicate<ConstructorInfo>> filters = new[] {filter};
+
+            var ctors = typeof (CtorTest).GetConstructors();
+
+            var result = ctors.Where(ctor => !filters.Any(f => f(ctor)))
+                .OrderBy(ctor => ctor.GetParameters().Length);
+
+            //must find CtorTest(ILogger) and CtorTest(string,ILogger)
+            //the other two have primitive parameters that are not satisfied
+            Assert.AreEqual(2, result.Count());
+            Assert.AreEqual(1, result.First().GetParameters().Length);
+            Assert.AreEqual(2, result.ElementAt(1).GetParameters().Length);
+        }
+
+        [TestMethod]
+        public void WhenFilterByArgTypesFitFindsExpectedCtors()
+        {
+            ParameterMatcher matcher = new ParameterMatcher(
+                new Dictionary<string, object>
+                    {
+                        {"connectionString", Constants.ConnectionStringValue},
+                        {"anotherParam", "I'm a string"}
+                    });
+
+            Predicate<ConstructorInfo> filter = matcher.FilterByArgTypesFit;
+
+            IEnumerable<Predicate<ConstructorInfo>> filters = new[] {filter};
+
+            var ctors = typeof (CtorTest).GetConstructors();
+
+            var result = ctors.Where(ctor => !filters.Any(f => f(ctor)))
+                .OrderBy(ctor => ctor.GetParameters().Length);
+
+            //must find CtorTest(string,ILogger,string), CtorTest(string,ILogger) and CtorTest(ILogger)
+            //ctor with one and two args do not take any argument whose type does not match the
+            //type of any provided arg
+            Assert.AreEqual(3, result.Count());
+            Assert.AreEqual(1, result.First().GetParameters().Length);
+            Assert.AreEqual(2, result.ElementAt(1).GetParameters().Length);
+            Assert.AreEqual(3, result.ElementAt(2).GetParameters().Length);
+        }
+    }
+
+    internal class CtorTest : ICtorTest
+    {
+        public CtorTest(ILogger logger)
+        {
+            Assert.Fail("must not be called");
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CtorTest"/> class
+        /// </summary>
+        public CtorTest(string connectionString, ILogger logger)
+        {
+            Assert.AreEqual("blablub", connectionString);
+            Assert.IsNotNull(logger);
+        }
+
+        public CtorTest(string connectionString, ILogger logger, int anotherParam)
+        {
+            Assert.Fail("must not be selected");
+        }
+
+        public CtorTest(string connectionString, ILogger logger, string anotherParam)
+        {
+            Assert.AreEqual("blablub", connectionString);
+            Assert.IsNotNull(logger);
+            Assert.AreEqual("I'm a string", anotherParam);
+        }
+    }
+
+    internal interface ICtorTest
+    {
+    }
+}
---
+++ src/TecX/TecX.Unity/ClozeInjectionConstructur.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+using Microsoft.Practices.ObjectBuilder2;
+using Microsoft.Practices.Unity;
+using Microsoft.Practices.Unity.ObjectBuilder;
+
+using TecX.Common;
+
+namespace TecX.Unity
+{
+    public class ClozeInjectionConstructur : InjectionMember
+    {
+        #region Fields
+
+        private readonly IDictionary<string, object> _ctorArguments;
+
+        #endregion Fields
+
+        #region c'tor
+
+        private ClozeInjectionConstructur()
+        {
+            _ctorArguments = new Dictionary<string, object>();
+        }
+
+        public ClozeInjectionConstructur(string argName, object argValue)
+            : this()
+        {
+            Guard.AssertNotEmpty(argName, "argName");
+            Guard.AssertNotNull(argValue, "argValue");
+
+            _ctorArguments.Add(argName, argValue);
+        }
+
+        public ClozeInjectionConstructur(IEnumerable<KeyValuePair<string, object>> ctorArguments)
+            : this()
+        {
+            Guard.AssertNotNull(ctorArguments, "ctorArguments");
+
+            foreach (var ctorArgument in ctorArguments)
+            {
+                _ctorArguments.Add(ctorArgument);
+            }
+        }
+
+        #endregion c'tor
+
+        #region Methods
+
+        public override void AddPolicies(Type serviceType, Type implementationType, string name,
+                                         IPolicyList policies)
+        {
+            ConstructorInfo ctor = FindConstructor(implementationType);
+
+            InjectionParameterValue[] parameterValues = GetParameterValues(ctor);
+
+            policies.Set<IConstructorSelectorPolicy>(
+                new SpecifiedConstructorSelectorPolicy(ctor, parameterValues),
+                new NamedTypeBuildKey(implementationType, name));
+        }
+
+        private InjectionParameterValue[] GetParameterValues(ConstructorInfo ctor)
+        {
+            ParameterInfo[] infos = ctor.GetParameters();
+
+            object[] parameterValues = new object[infos.Length];
+
+            //fill in the provided values and add the type of the parameter that the
+            //ctor expects otherwise. unity will take care of resolving parameters that
+            //are provided that way
+            for (int i = 0; i < infos.Length; i++)
+            {
+                object value;
+                if (_ctorArguments.TryGetValue(infos[i].Name, out value))
+                {
+                    parameterValues[i] = value;
+                }
+                else
+                {
+                    parameterValues[i] = infos[i].ParameterType;
+                }
+            }
+
+            return InjectionParameterValue.ToParameters(parameterValues).ToArray();
+        }
+
+        private ConstructorInfo FindConstructor(Type typeToCreate)
+        {
+            var ctors = typeToCreate.GetConstructors();
+
+            ParameterMatcher matcher = new ParameterMatcher(_ctorArguments);
+
+            return matcher.BestMatch(ctors);
+        }
+
+        #endregion Methods
+    }
+}
