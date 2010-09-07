d5b436783d305c490a0b4b4e3291afe9aab6c263
Damien80 <Damien80@localhost>
2010-09-07 18:57:52 +0000

    lets get started

---
+++ src/TecX/TecX.Unity.Test/UnityContainerExtensionRegistrationFixture.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TecX.Unity.Test
+{
+    [TestClass]
+    public class UnityContainerExtensionRegistrationFixture
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
