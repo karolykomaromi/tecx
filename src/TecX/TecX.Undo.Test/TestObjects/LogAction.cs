7bae36f5ab5223e2ab5af50003fb58cdc8048e41
Damien80 <Damien80@localhost>
2010-09-30 19:56:21 +0000

    remove old undo project

---
+++ src/TecX/TecX.Undo.Test/TestObjects/LogAction.cs
@@ -0,0 +1,18 @@
+﻿namespace TecX.Undo.Test.TestObjects
+{
+    public class LogAction : AbstractAction
+    {
+        public int ExecutesCount { get; set; }
+        public int UnexecutesCount { get; set; }
+
+        protected override void ExecuteCore()
+        {
+            ExecuteCount++;
+        }
+
+        protected override void UnExecuteCore()
+        {
+            UnexecutesCount++;
+        }
+    }
+}
---
+++ src/TecX/TecX.Undo.Test/TestObjects/ThrowingAction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TecX.Undo.Test.TestObjects
+{
+    public class ThrowingAction : AbstractAction
+    {
+        protected override void ExecuteCore()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void UnExecuteCore()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
