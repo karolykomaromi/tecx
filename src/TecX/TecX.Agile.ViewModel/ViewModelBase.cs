4cdaa5be6cd6fbc620d6db076d25b6e5c4c3fb17
Damien80 <Damien80@localhost>
2010-10-01 10:58:08 +0000

    refining undo, added viewmodel assembly

---
+++ src/TecX/TecX.Agile.ViewModel/ViewModelBase.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using TecX.Common;
+
+namespace TecX.Agile.ViewModel
+{
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+
+        protected void OnPropertyChanged<T>(Expression<Func<T>> propertySelector)
+        {
+            Guard.AssertNotNull(propertySelector, "propertySelector");
+
+            string propertyName = GetPropertyName(propertySelector);
+
+            OnPropertyChanged(propertyName);
+        }
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            Guard.AssertNotEmpty(propertyName, "propertyName");
+
+            if(PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        private static string GetPropertyName<T>(Expression<Func<T>> propertySelector)
+        {
+            Guard.AssertNotNull(propertySelector, "propertySelector");
+
+            MemberExpression member = propertySelector.Body as MemberExpression;
+
+            if (member != null)
+            {
+                return member.Member.Name;
+            }
+
+            return string.Empty;
+        }
+
+    }
+}
