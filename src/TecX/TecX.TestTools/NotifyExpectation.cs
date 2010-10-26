eed1056a3cf12541e3d359d543f6140dd330c1ae
Damien80 <Damien80@localhost>
2010-10-26 08:29:52 +0000

    extended fluent notification api to verify eventargs

--- src/TecX/TecX.TestTools/NotifyExpectation.cs
+++ src/TecX/TecX.TestTools/NotifyExpectation.cs
@@ -1,10 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading;
-
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-
 using TecX.Common;
 
 namespace TecX.TestTools
@@ -15,8 +11,6 @@ namespace TecX.TestTools
 
         private readonly T _owner;
         private readonly Action<T> _action;
-        private bool _eventRaised;
-        private readonly ManualResetEvent _manualResetEvent;
 
         #endregion Fields
 
@@ -29,24 +23,21 @@ namespace TecX.TestTools
 
             _owner = owner;
             _action = action;
-
-            _eventRaised = false;
-            _manualResetEvent = new ManualResetEvent(false);
         }
 
         #endregion c'tor
 
-        public void ShouldNotifyVia<TEventArgs>(string eventName)
+        public EventExpectation<T, TEventArgs> ShouldNotifyVia<TEventArgs>(string eventName)
             where TEventArgs : EventArgs
         {
             Guard.AssertNotEmpty(eventName, "eventName");
 
-            var observable = Observable.FromEvent<TEventArgs>(_owner, eventName);
+            IObservable<IEvent<TEventArgs>> observable = Observable.FromEvent<TEventArgs>(_owner, eventName);
 
-            AssertNotified(observable);
+            return new EventExpectation<T, TEventArgs>(_owner, _action, observable);
         }
 
-        public void ShouldNotifyVia<TDelegate, TEventArgs>(Func<EventHandler<TEventArgs>, TDelegate> conversion,
+        public EventExpectation<T, TEventArgs> ShouldNotifyVia<TDelegate, TEventArgs>(Func<EventHandler<TEventArgs>, TDelegate> conversion,
                                                            Action<TDelegate> addHandler, Action<TDelegate> removeHandler)
             where TEventArgs : EventArgs
         {
@@ -56,59 +47,7 @@ namespace TecX.TestTools
 
             IObservable<IEvent<TEventArgs>> observable = Observable.FromEvent(conversion, addHandler, removeHandler);
 
-            AssertNotified(observable);
+            return new EventExpectation<T, TEventArgs>(_owner, _action, observable);
         }
-
-        private void AssertNotified<TEventArgs>(IObservable<IEvent<TEventArgs>> observable)
-            where TEventArgs : EventArgs
-        {
-            _eventRaised = false;
-
-            using (observable.Subscribe(e =>
-                                            {
-                                                _eventRaised = true;
-                                                _manualResetEvent.Set();
-                                            }))
-            {
-                _action(_owner);
-                _manualResetEvent.WaitOne(1500, false);
-            }
-
-            Assert.IsTrue(_eventRaised);
-        }
-
-        //public void ShouldNotifyVia<TEventArgs>(Action<T> addOrRemoveHandler)
-        //    where TEventArgs : EventArgs
-        //{
-        //    if (addOrRemoveHandler == null) throw new ArgumentException("addHandler");
-
-        //    string eventName = GetEventName(addOrRemoveHandler);
-
-        //    ShouldNotifyVia<TEventArgs>(eventName);
-        //}
-
-        //private static string GetEventName(Action<T> addHandler)
-        //{
-        //    Globals.LoadOpCodes();
-        //    var mr = new MethodBodyReader(addHandler.Method);
-        //    var i = mr.instructions;
-
-        //    foreach (var ilInstruction in i)
-        //    {
-        //        var methodInfo = ilInstruction.Operand as System.Reflection.MethodInfo;
-        //        if (ilInstruction.Code.Name != "callvirt" || methodInfo == null)
-        //        {
-        //            continue;
-        //        }
-
-        //        var name = methodInfo.Name;
-        //        if (name.StartsWith("add_"))
-        //        {
-        //            return name.Substring(4);
-        //        }
-        //    }
-
-        //    return string.Empty;
-        //}
     }
 }
