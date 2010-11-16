44a47706e0a9395e4e2f4a4dfd820ab652140072
Damien80 <Damien80@localhost>
2010-11-16 21:17:45 +0000

    undo redo engine listens to eventaggregator and triggers actions on incoming events

--- src/TecX/TecX.Agile.ViewModel/ChangeTracking/PropertyChangeSubscription.cs
+++ src/TecX/TecX.Agile.ViewModel/ChangeTracking/PropertyChangeSubscription.cs
@@ -38,23 +38,6 @@ namespace TecX.Agile.ViewModel.ChangeTracking
                                                                })
                 .Where(ba => ba.NewValue != ba.OldValue);
 
-
-            ////TODO weberse instead of hard wiring a single handler we can use some kind of ChangeHandlerChain that contains
-            ////different handlers (e.g. for P2P distribution of changed properties, tracing information or the like)
-            //var handlerChain = new PropertyChangeHandlerChain();
-            //handlerChain.Add((parentObject, propertyName, oldValue, newValue) =>
-            //                     {
-            //                         var action = new SetPropertyAction(parentObject,
-            //                                                            propertyName,
-            //                                                            oldValue,
-            //                                                            newValue);
-
-            //                         _actionManager.RecordAction(action);
-            //                     });
-
-            //_subscription = beforeAfter.Subscribe(x => handlerChain
-            //    .Handle(x.ParentObject, x.PropertyName, x.OldValue, x.NewValue));
-
             _subscription =
                 beforeAfter.Subscribe(x => _eventAggregator.Publish(
                                                     new PropertyChanged(
