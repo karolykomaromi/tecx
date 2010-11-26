eb26a783261d8518a6e9355e83607202d3843405
Damien80 <Damien80@localhost>
2010-11-26 10:06:37 +0000

    first attempts to tweak the ui

---
+++ src/TecX/TecX.Agile.ViewModel/Constants.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Media;
+
+namespace TecX.Agile.ViewModel
+{
+    public static class Constants
+    {
+        public static class Colors
+        {
+            /// <summary>#FF66CDAA</summary>
+            public static readonly Color Aqua = Color.FromRgb(102, 205, 170);
+            /// <summary>#FFADD8E6</summary>
+            public static readonly Color Blue = Color.FromRgb(173, 216, 230);
+            /// <summary>#FFBEBEBE</summary>
+            public static readonly Color Gray = Color.FromRgb(190, 190, 190);
+            /// <summary>#FF8FBC8F</summary>
+            public static readonly Color Green = Color.FromRgb(143, 188, 143);
+            /// <summary>#FFBDB76B</summary>
+            public static readonly Color Khaki = Color.FromRgb(189, 183, 107);
+            /// <summary>#FFFFDAB9</summary>
+            public static readonly Color Peach = Color.FromRgb(255, 218, 185);
+            /// <summary>#FFFFC0CB</summary>
+            public static readonly Color Pink = Color.FromRgb(255, 192, 203);
+            /// <summary>#FFFF6347</summary>
+            public static readonly Color Red = Color.FromRgb(255, 99, 71);
+            /// <summary>#FFFFF68F</summary>
+            public static readonly Color Yellow = Color.FromRgb(255, 246, 143);
+            /// <summary>#BF9AFF00</summary>
+            public static readonly Color CardSelected = Color.FromArgb(191, 154, 255, 0);
+            /// <summary>#FFFFFFFF</summary>
+            public static readonly Color White = System.Windows.Media.Colors.White;
+            /// <summary>#FF9ACD32</summary>
+            public static readonly Color YellowGreen = Color.FromArgb(255, 154, 205, 50);
+        }
+    }
+}
--- src/TecX/TecX.Agile.ViewModel/StoryCard.cs
+++ src/TecX/TecX.Agile.ViewModel/StoryCard.cs
@@ -1,12 +1,44 @@
 ﻿using System;
-
-using TecX.Common;
+using System.Windows.Media;
 
 namespace TecX.Agile.ViewModel
 {
     [Serializable]
     public class StoryCard : PlanningArtefact
     {
+        #region Fields
+
+        private Color _background;
+
+        #endregion Fields
+
+        #region Properties
+
         public PlanningArtefactCollection<StoryCard> Parent { get; internal set; }
+
+        public Color Background
+        {
+            get { return _background; }
+            set
+            {
+                if (_background == value)
+                    return;
+
+                OnPropertyChanging(() => Background);
+                _background = value;
+                OnPropertyChanged(() => Background);
+            }
+        }
+
+        #endregion Properties
+
+        #region c'tor
+
+        public StoryCard()
+        {
+            _background = Constants.Colors.Yellow;
+        }
+
+        #endregion c'tor
     }
 }
