0bdc403e89737c179bbb508665238f96fe413f79
Damien80 <Damien80@localhost>
2010-11-30 14:06:44 +0000

    looks like a lot of work to port that stuff from outlawtrail to tecx...

--- src/TecX/TecX.Agile.View/Behavior/MovementBehaviorBase.cs
+++ src/TecX/TecX.Agile.View/Behavior/MovementBehaviorBase.cs
@@ -2,12 +2,46 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows;
+
+using TecX.Common;
 
 namespace TecX.Agile.View.Behavior
 {
     public class MovementBehaviorBase : BehaviorBase
     {
-        //pinned
+        /// <summary>
+        /// Value indicating wether a <see cref="UIElement"/> should be considered
+        /// as immobile. This is a <see cref="DependencyProperty"/>
+        /// </summary>
+        public static readonly DependencyProperty IsPinnedProperty =
+            DependencyProperty.RegisterAttached(
+                "IsPinned",
+                typeof(bool),
+                typeof(MovementBehaviorBase),
+                new FrameworkPropertyMetadata(false));
+
+
+        /// <summary>
+        /// Getter for <see cref="DependencyProperty"/> <see cref="IsPinnedProperty"/>
+        /// </summary>
+        public static bool GetIsPinned(DependencyObject dependencyObject)
+        {
+            Guard.AssertNotNull(dependencyObject, "dependencyObject");
+
+            return (bool)dependencyObject.GetValue(IsPinnedProperty);
+        }
+
+        /// <summary>
+        /// Setter for <see cref="DependencyProperty"/> <see cref="IsPinnedProperty"/>
+        /// </summary>
+        public static void SetIsPinned(DependencyObject dependencyObject, bool value)
+        {
+            Guard.AssertNotNull(dependencyObject, "dependencyObject");
+
+            dependencyObject.SetValue(IsPinnedProperty, value);
+        }
+
 
         //transformgroup rotate, scale, translate
     }
