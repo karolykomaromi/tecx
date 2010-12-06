fe4ae67fe2946d00484451dde1f835a8a2a63ea8
Damien80 <Damien80@localhost>
2010-12-06 12:33:30 +0000

    try to extract transform information from matrix

---
+++ src/TecX/TecX.Common/Comparison/EpsilonComparer.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TecX.Common.Comparison
+{
+    public class EpsilonComparer : IComparer<double>
+    {
+        private static class Constants
+        {
+            /// <summary>0.0000000001</summary>
+            public const double Epsilon = 0.00000000000001;
+        }
+
+        #region Implementation of IComparer<in double>
+
+        int IComparer<double>.Compare(double x, double y)
+        {
+            return Compare(x, y);
+        }
+
+        #endregion
+
+        public static int Compare(double x, double y)
+        {
+            if (Math.Abs(x - y) <= x * Constants.Epsilon)
+            {
+                return 0;
+            }
+
+            return x < y ? -1 : 1;
+        }
+
+        public static bool AreEqual(double x, double y)
+        {
+            return Compare(x, y) == 0;
+        }
+
+        public static bool IsGreaterThan(double x, double y)
+        {
+            return Compare(x, y) > 0;
+        }
+
+        public static bool IsGreaterThanOrEqual(double x, double y)
+        {
+            return Compare(x, y) >= 0;
+        }
+
+        public static bool IsLessThan(double x, double y)
+        {
+            return Compare(x, y) < 0;
+        }
+
+        public static bool IsLessThanOrEqual(double x, double y)
+        {
+            return Compare(x, y) <= 0;
+        }
+
+        public static bool IsAlmostZero(double x)
+        {
+            return AreEqual(x, 0.0);
+        }
+    }
+}
