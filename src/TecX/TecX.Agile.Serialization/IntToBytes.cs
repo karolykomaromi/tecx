2a62ff3a1f15c5d7610877ad2a19d93bb08654b1
Damien80 <Damien80@localhost>
2010-12-20 16:03:29 +0000

    as i cant work on tfs from here i started with binary serializing messages i want to push out so silverlight via sockets

---
+++ src/TecX/TecX.Agile.Serialization/IntToBytes.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace TecX.Agile.Serialization
+{
+    /// <summary>
+    /// The "IntToBytes" struct call basically takes care of writing the length of the serialized stream which 
+    /// follows into the first four bytes - a very common construct in creating streams of bytes that are 
+    /// "self - describing".
+    /// </summary>
+    [StructLayout(LayoutKind.Explicit)]
+    internal struct IntToBytes
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IntToBytes"/> struct.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        public IntToBytes(Int32 value)
+        {
+            Byte0 = Byte1 = Byte2 = Byte3 = 0;
+            Int32 = value;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IntToBytes"/> struct.
+        /// </summary>
+        /// <param name="b0">The first byte.</param>
+        /// <param name="b1">The second byte.</param>
+        /// <param name="b2">The third byte.</param>
+        /// <param name="b3">The fourth byte.</param>
+        public IntToBytes(byte b0, byte b1, byte b2, byte b3)
+        {
+            Int32 = 0;
+            Byte0 = b0;
+            Byte1 = b1;
+            Byte2 = b2;
+            Byte3 = b3;
+        }
+
+        [FieldOffset(0)]
+        public Int32 Int32;
+
+        [FieldOffset(0)]
+        public byte Byte0;
+
+        [FieldOffset(1)]
+        public byte Byte1;
+
+        [FieldOffset(2)]
+        public byte Byte2;
+
+        [FieldOffset(3)]
+        public byte Byte3;
+    }
+}
---
+++ src/TecX/TecX.Agile.Serialization/Messages/PropertyUpdated.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+
+using TecX.Common;
+
+namespace TecX.Agile.Serialization.Messages
+{
+    public class PropertyUpdated : IBinarySerializable
+    {
+        public string PropertyName { get; set; }
+        public Guid ArtefactId { get; set; }
+        public object OldValue { get; set; }
+        public object NewValue { get; set; }
+
+        public PropertyUpdated()
+        {
+            PropertyName = string.Empty;
+            ArtefactId = Guid.Empty;
+            OldValue = null;
+            NewValue = null;
+        }
+
+        #region Implementation of IBinarySerializable
+
+        public void WriteDataTo(BinaryWriter writer)
+        {
+            Guard.AssertNotNull(writer, "writer");
+
+            writer.Write(ArtefactId.ToByteArray());
+            writer.Write(PropertyName);
+
+            if (OldValue == null)
+                writer.Write(Constants.DataTypes.Null);
+
+            if (NewValue == null)
+                writer.Write(Constants.DataTypes.Null);
+
+        }
+
+        public void SetDataFrom(BinaryReader reader)
+        {
+            Guard.AssertNotNull(reader, "reader");
+
+            ArtefactId = new Guid(reader.ReadBytes(16));
+            PropertyName = reader.ReadString();
+
+            byte dataType = reader.ReadByte();
+
+            if(dataType == Constants.DataTypes.Null)
+            {
+                OldValue = null;
+            }
+
+            dataType = reader.ReadByte();
+
+            if(dataType == Constants.DataTypes.Null)
+            {
+                NewValue = null;
+            }
+        }
+
+        public int GetTypeId()
+        {
+            return Constants.TypeIds.PropertyUpdated;
+        }
+
+        #endregion
+    }
+}
