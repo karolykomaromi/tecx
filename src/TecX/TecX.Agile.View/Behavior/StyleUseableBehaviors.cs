07191c75386492678f872f142798c84958e94dd1
Damien80 <Damien80@localhost>
2011-01-07 14:18:21 +0000

    new styleuseablebehaviors

--- src/TecX/TecX.Agile.View/Behavior/StyleUseableBehaviors.cs
+++ src/TecX/TecX.Agile.View/Behavior/StyleUseableBehaviors.cs
@@ -1,6 +1,8 @@
 ﻿using System.Windows;
 using System.Windows.Interactivity;
 
+using TecX.Common;
+
 namespace TecX.Agile.View.Behavior
 {
     public class StyleUseableBehaviors
@@ -13,28 +15,76 @@ namespace TecX.Agile.View.Behavior
             typeof(StyleUseableBehaviors),
             new FrameworkPropertyMetadata(null, OnPropertyChanged));
 
-        public static StyleUseableBehaviorCollection GetBehaviors(DependencyObject uie)
+        public static StyleUseableBehaviorCollection GetBehaviors(DependencyObject obj)
         {
-            return (StyleUseableBehaviorCollection)uie.GetValue(BehaviorsProperty);
+            Guard.AssertNotNull(obj, "obj");
+
+            return (StyleUseableBehaviorCollection)obj.GetValue(BehaviorsProperty);
         }
 
-        public static void SetBehaviors(DependencyObject uie, StyleUseableBehaviorCollection value)
+        public static void SetBehaviors(DependencyObject obj, StyleUseableBehaviorCollection value)
         {
-            uie.SetValue(BehaviorsProperty, value);
+            Guard.AssertNotNull(obj, "obj");
+
+            obj.SetValue(BehaviorsProperty, value);
+        }
+
+        private static readonly DependencyProperty OriginalBehaviorProperty = DependencyProperty.RegisterAttached(
+            @"OriginalBehaviorInternal",
+            typeof(System.Windows.Interactivity.Behavior),
+            typeof(StyleUseableBehaviors),
+            new UIPropertyMetadata(null));
+
+        private static System.Windows.Interactivity.Behavior GetOriginalBehavior(DependencyObject obj)
+        {
+            Guard.AssertNotNull(obj, "obj");
+
+            return obj.GetValue(OriginalBehaviorProperty) as System.Windows.Interactivity.Behavior;
+        }
+
+        private static void SetOriginalBehavior(DependencyObject obj, System.Windows.Interactivity.Behavior value)
+        {
+            Guard.AssertNotNull(obj, "obj");
+
+            obj.SetValue(OriginalBehaviorProperty, value);
         }
 
         #endregion DependencyProperties
 
-        #region EventHandling
+        private static int GetIndexOf(BehaviorCollection itemBehaviors, System.Windows.Interactivity.Behavior behavior)
+        {
+            int index = -1;
+
+            System.Windows.Interactivity.Behavior orignalBehavior = GetOriginalBehavior(behavior);
+
+            for (int i = 0; i < itemBehaviors.Count; i++)
+            {
+                System.Windows.Interactivity.Behavior currentBehavior = itemBehaviors[i];
+
+                if (currentBehavior == behavior ||
+                    currentBehavior == orignalBehavior)
+                {
+                    return i;
+                }
+
+                System.Windows.Interactivity.Behavior currentOrignalBehavior = GetOriginalBehavior(currentBehavior);
+
+                if (currentOrignalBehavior == behavior ||
+                    currentOrignalBehavior == orignalBehavior)
+                {
+                    return i;
+                }
+            }
+
+            return index;
+        }
 
         private static void OnPropertyChanged(DependencyObject dpo, DependencyPropertyChangedEventArgs e)
         {
             var uie = dpo as UIElement;
 
             if (uie == null)
-            {
                 return;
-            }
 
             BehaviorCollection itemBehaviors = Interaction.GetBehaviors(uie);
 
@@ -42,15 +92,13 @@ namespace TecX.Agile.View.Behavior
             var oldBehaviors = e.OldValue as StyleUseableBehaviorCollection;
 
             if (newBehaviors == oldBehaviors)
-            {
                 return;
-            }
 
             if (oldBehaviors != null)
             {
                 foreach (var behavior in oldBehaviors)
                 {
-                    int index = itemBehaviors.IndexOf(behavior);
+                    int index = GetIndexOf(itemBehaviors, behavior);
 
                     if (index >= 0)
                     {
@@ -61,18 +109,18 @@ namespace TecX.Agile.View.Behavior
 
             if (newBehaviors != null)
             {
-                foreach (var behavior in newBehaviors)
+                foreach (System.Windows.Interactivity.Behavior behavior in newBehaviors)
                 {
-                    int index = itemBehaviors.IndexOf(behavior);
+                    int index = GetIndexOf(itemBehaviors, behavior);
 
                     if (index < 0)
                     {
-                        itemBehaviors.Add(behavior);
+                        var clone = (System.Windows.Interactivity.Behavior)behavior.Clone();
+                        SetOriginalBehavior(clone, behavior);
+                        itemBehaviors.Add(clone);
                     }
                 }
             }
         }
-
-        #endregion EventHandling
     }
 }
