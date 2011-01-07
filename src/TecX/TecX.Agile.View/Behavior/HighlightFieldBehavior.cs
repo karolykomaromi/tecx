6408d0bb46ba88c4cd6173271c4e5a96faabc048
Damien80 <Damien80@localhost>
2011-01-07 14:18:21 +0000

    new styleuseablebehaviors

--- src/TecX/TecX.Agile.View/Behavior/HighlightFieldBehavior.cs
+++ src/TecX/TecX.Agile.View/Behavior/HighlightFieldBehavior.cs
@@ -1,74 +1,20 @@
 ﻿using System;
 using System.ComponentModel;
-using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
-using System.Windows.Interactivity;
 
 using Microsoft.Practices.Prism.Commands;
 
 using TecX.Agile.Infrastructure;
 using TecX.Agile.Infrastructure.Events;
 using TecX.Agile.ViewModel;
-using TecX.Common;
 using TecX.Common.Event;
 
 namespace TecX.Agile.View.Behavior
 {
     public class HighlightFieldBehavior : System.Windows.Interactivity.Behavior<TextBox>
     {
-        #region DependencyProperties
-
-        public static readonly DependencyProperty IsEnabledProperty = DependencyProperty.RegisterAttached(
-            "IsEnabled",
-            typeof (bool),
-            typeof (HighlightFieldBehavior),
-            new FrameworkPropertyMetadata(false, FrameworkPropertyMetadataOptions.None, OnEnabledChanged));
-
-        public static bool GetIsEnabled(TextBox tb)
-        {
-            Guard.AssertNotNull(tb, "tb");
-
-            return (bool) tb.GetValue(IsEnabledProperty);
-        }
-
-        public static void SetIsEnabled(TextBox tb, bool isEnabled)
-        {
-            Guard.AssertNotNull(tb, "tb");
-
-            tb.SetValue(IsEnabledProperty, isEnabled);
-        }
-
-        private static void OnEnabledChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
-        {
-            if (LicenseManager.UsageMode == LicenseUsageMode.Designtime)
-                return;
-
-            TextBox tb = d as TextBox;
-
-            if (tb == null)
-                return;
-
-            var behaviors = Interaction.GetBehaviors(tb);
-
-            if (e.NewValue is bool == false)
-            {
-                foreach (var pfb in behaviors.ToArray().OfType<HighlightFieldBehavior>())
-                {
-                    behaviors.Remove(pfb);
-                }
-            }
-            else
-            {
-                var behavior = new HighlightFieldBehavior();
-
-                behaviors.Add(behavior);
-            }
-        }
-
-        #endregion DependencyProperties
-
         #region Fields
 
         private Guid _id;
--- src/TecX/TecX.Agile.View/Behavior/PreviewFocusBehavior.cs
+++ src/TecX/TecX.Agile.View/Behavior/PreviewFocusBehavior.cs
@@ -1,11 +1,6 @@
 ﻿using System.ComponentModel;
-using System.Linq;
-using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
-using System.Windows.Interactivity;
-
-using TecX.Common;
 
 namespace TecX.Agile.View.Behavior
 {
@@ -16,58 +11,6 @@ namespace TecX.Agile.View.Behavior
     /// </summary>
     public class PreviewFocusBehavior : System.Windows.Interactivity.Behavior<TextBox>
     {
-        #region DependencyProperties
-
-        public static readonly DependencyProperty IsEnabledProperty = DependencyProperty.RegisterAttached(
-            "IsEnabled",
-            typeof(bool),
-            typeof(PreviewFocusBehavior),
-            new FrameworkPropertyMetadata(false, FrameworkPropertyMetadataOptions.None, OnEnabledChanged));
-
-        public static bool GetIsEnabled(TextBox tb)
-        {
-            Guard.AssertNotNull(tb, "tb");
-
-            return (bool)tb.GetValue(IsEnabledProperty);
-        }
-
-        public static void SetIsEnabled(TextBox tb, bool isEnabled)
-        {
-            Guard.AssertNotNull(tb, "tb");
-
-            tb.SetValue(IsEnabledProperty, isEnabled);
-        }
-
-        private static void OnEnabledChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
-        {
-            if (LicenseManager.UsageMode == LicenseUsageMode.Designtime)
-                return;
-
-            TextBox tb = d as TextBox;
-
-            if (tb == null)
-                return;
-
-            var behaviors = Interaction.GetBehaviors(tb);
-
-            if(e.NewValue is bool == false)
-            {
-                foreach (var pfb in behaviors.ToArray().OfType<PreviewFocusBehavior>())
-                {
-                    behaviors.Remove(pfb);
-                }
-            }
-            else
-            {
-                var behavior = new PreviewFocusBehavior();
-
-                behaviors.Add(behavior);  
-            }
-        }
-
-        #endregion DependencyProperties
-
-
         protected override void OnAttached()
         {
             base.OnAttached();
