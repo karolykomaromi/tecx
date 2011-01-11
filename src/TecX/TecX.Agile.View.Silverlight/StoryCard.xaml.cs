99089733bc27f292ecb59fe8778adb99eade0864
Damien80 <Damien80@localhost>
2011-01-11 11:56:13 +0000

    silverlight card looks almost the same as wpf card.

--- src/TecX/TecX.Agile.View.Silverlight/StoryCard.xaml.cs
+++ src/TecX/TecX.Agile.View.Silverlight/StoryCard.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
 
 using TecX.Common;
 
@@ -20,7 +22,6 @@ namespace TecX.Agile.View
             DataContext = storyCard;
 
             InitializeComponent();
-
         }
 
         #endregion c'tor
