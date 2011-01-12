8c5e7d3031a7e5046ade80677c83e75019044c40
Damien80 <Damien80@localhost>
2011-01-12 15:26:09 +0000

    trying to get a wcf service playing nicely with silverlight is a real pita

---
+++ src/TecX/TecX.Agile.Planner.Silverlight.Web/Service/CustomServiceHostFactory.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.Threading;
+using System.Web;
+
+using TecX.Agile.Peer;
+using TecX.Agile.Push;
+
+namespace TecX.Agile.Planner.Silverlight.Web.Service
+{
+    public class CustomServiceHostFactory : ServiceHostFactory
+    {
+        private readonly Thread _socketServerThread;
+        private readonly Thread _policyServerThread;
+
+        private readonly ISocketServer _socketServer;
+        private readonly PolicySocketServer _policySocketServer;
+        private readonly IPeerClient _peerClient;
+
+        public CustomServiceHostFactory()
+        {
+            _policySocketServer = new PolicySocketServer();
+            _socketServer = new SocketServer();
+            _peerClient = new PeerClient();
+
+            _policyServerThread = new Thread(_policySocketServer.Start);
+            _socketServerThread = new Thread(_socketServer.Start);
+        }
+
+        protected override ServiceHost CreateServiceHost(Type serviceType, Uri[] baseAddresses)
+        {
+            if(serviceType == typeof(SilverlightPlanningService))
+            {
+                return new CustomServiceHost(_socketServer, _peerClient, serviceType, baseAddresses);
+            }
+
+            return base.CreateServiceHost(serviceType, baseAddresses);
+        }
+    }
+}
