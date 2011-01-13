03ae6f91ee73248eb78d2d461d98ae1e48d5ef48
Damien80 <Damien80@localhost>
2011-01-13 13:48:08 +0000

    idea how to realize services like displaying text and showing viewmodels on the surface

---
+++ src/TecX/TecX.Agile.Modules.Main/Services/DisplayTextService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using TecX.Agile.Infrastructure.Services;
+
+namespace TecX.Agile.Modules.Main.Services
+{
+    public class DisplayTextService : IDisplayText
+    {
+        public void Show(string text)
+        {
+            
+        }
+    }
+}
---
+++ src/TecX/TecX.Agile.Modules.Main/Services/ShowViewModelService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using TecX.Agile.Infrastructure.Services;
+
+namespace TecX.Agile.Modules.Main.Services
+{
+    public class ShowViewModelService : IShowViewModels
+    {
+        public void Show(object viewModel, string regionName)
+        {
+            
+        }
+    }
+}
