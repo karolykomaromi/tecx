da8e6e5f9a07714eb33ed2b8c9b5dad653396109
Damien80 <Damien80@localhost>
2011-01-13 09:31:00 +0000

    how to shoot yourself in the foot in 347 easy steps ... using the wcf service in the silverlight.web project a forwarder to a real implementation hosted in a console app. using namedpipes to get messages from sl-service to console-service

--- src/TecX/TecX.Agile.Planner.Silverlight.Web/SilverlightPlanningService.svc.cs
+++ src/TecX/TecX.Agile.Planner.Silverlight.Web/SilverlightPlanningService.svc.cs
@@ -1,194 +1,47 @@
 ﻿using System;
+using System.ServiceModel;
 using System.ServiceModel.Activation;
-
-using TecX.Agile.Peer;
-using TecX.Agile.Push;
-using TecX.Agile.Serialization;
-using TecX.Agile.Serialization.Messages;
+using System.ServiceModel.Channels;
 using TecX.Agile.Server;
-using TecX.Common;
 
 namespace TecX.Agile.Silverlight.Service
 {
-    //[AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
     public class SilverlightPlanningService : ISilverlightPlanningService
     {
-        #region Fields
+        private readonly ISilverlightPlanningService _proxy;
 
-        private readonly IPeerClient _peerClient;
-        private readonly ISocketServer _socketServer;
-        private readonly BinaryFormatter _formatter;
-
-        #endregion Fields
-
-        #region c'tor
-
-        public SilverlightPlanningService(ISocketServer socketServer, IPeerClient peerClient)
+        public SilverlightPlanningService()
         {
-            Guard.AssertNotNull(socketServer, "socketServer");
-            Guard.AssertNotNull(peerClient, "peerClient");
+            Binding binding = new NetNamedPipeBinding();
 
-            _socketServer = socketServer;
-            _peerClient = peerClient;
+            EndpointAddress address = new EndpointAddress(Constants.DefaultEndPointAddress);
 
-            _formatter = new BinaryFormatter();
+            ChannelFactory<ISilverlightPlanningService> factory = new ChannelFactory<ISilverlightPlanningService>(binding, address);
 
-            InitializeBinaryFormatter();
+            ISilverlightPlanningService proxy = factory.CreateChannel();
 
-            _peerClient.PropertyUpdated += OnPropertyUpdated;
-            _peerClient.IncomingRequestToHighlightField += OnIncomingRequestToHighlightField;
-            _peerClient.CaretMoved += OnCaretMoved;
-            _peerClient.StoryCardMoved += OnStoryCardMoved;
+            _proxy = proxy;
         }
-
-        private void InitializeBinaryFormatter()
-        {
-            _formatter.Register<StoryCardMoved>(Serialization.Constants.MessageTypeIds.StoryCardMoved);
-            _formatter.Register<PropertyUpdated>(Serialization.Constants.MessageTypeIds.PropertyUpdated);
-            _formatter.Register<CaretMoved>(Serialization.Constants.MessageTypeIds.CaretMoved);
-            _formatter.Register<FieldHighlighted>(Serialization.Constants.MessageTypeIds.FieldHighlighted);
-        }
-
-        #endregion c'tor
-
-        #region EventHandling IPeerClient
-
-        private void OnStoryCardMoved(object sender, StoryCardMovedEventArgs e)
-        {
-            StoryCardMoved message = new StoryCardMoved
-            {
-                SenderId = e.SenderId,
-                StoryCardId = e.StoryCardId,
-                X = e.X,
-                Y = e.Y,
-                Angle = e.Angle
-            };
-
-            byte[] serialized = _formatter.Serialize(message);
-
-            _socketServer.Push(serialized);
-        }
-
-        private void OnCaretMoved(object sender, CaretMovedEventArgs e)
-        {
-            CaretMoved message = new CaretMoved
-            {
-                SenderId = e.SenderId,
-                ArtefactId = e.ArtefactId,
-                CaretIndex = e.CaretIndex,
-                FieldName = e.FieldName
-            };
-
-            byte[] serialized = _formatter.Serialize(message);
-
-            _socketServer.Push(serialized);
-        }
-
-        private void OnIncomingRequestToHighlightField(object sender, FieldHighlightedEventArgs e)
-        {
-            FieldHighlighted message = new FieldHighlighted
-            {
-                SenderId = e.SenderId,
-                ArtefactId = e.ArtefactId,
-                FieldName = e.FieldName
-            };
-
-            byte[] serialized = _formatter.Serialize(message);
-
-            _socketServer.Push(serialized);
-        }
-
-        private void OnPropertyUpdated(object sender, UpdatedPropertyEventArgs e)
-        {
-            PropertyUpdated message = new PropertyUpdated
-            {
-                SenderId = e.SenderId,
-                ArtefactId = e.ArtefactId,
-                PropertyName = e.PropertyName,
-                OldValue = e.OldValue,
-                NewValue = e.NewValue
-            };
-
-            byte[] serialized = _formatter.Serialize(message);
-
-            _socketServer.Push(serialized);
-        }
-
-        #endregion EventHandling IPeerClient
-
-        #region Incoming Calls from Silverlight Clients
 
         public void MoveStoryCard(Guid senderId, Guid storyCardId, double x, double y, double angle)
         {
-            Console.WriteLine("MoveStoryCard({0},{1},{2},{3},{4})", senderId, storyCardId, x, y, angle);
-
-            _peerClient.MoveStoryCard(senderId, storyCardId, x, y, angle);
-
-            StoryCardMoved message = new StoryCardMoved
-            {
-                SenderId = senderId,
-                StoryCardId = storyCardId,
-                X = x,
-                Y = y,
-                Angle = angle
-            };
-
-            byte[] serialized = _formatter.Serialize(message);
-
-            _socketServer.Push(serialized);
+            _proxy.MoveStoryCard(senderId, storyCardId, x, y, angle);
         }
 
         public void Highlight(Guid senderId, Guid artefactId, string fieldName)
         {
-            _peerClient.Highlight(senderId, artefactId, fieldName);
-
-            FieldHighlighted message = new FieldHighlighted
-            {
-                SenderId = senderId,
-                ArtefactId = artefactId,
-                FieldName = fieldName
-            };
-
-            byte[] serialized = _formatter.Serialize(message);
-
-            _socketServer.Push(serialized);
+            _proxy.Highlight(senderId, artefactId,fieldName);
         }
 
         public void UpdateProperty(Guid senderId, Guid artefactId, string propertyName, object oldValue, object newValue)
         {
-            _peerClient.UpdateProperty(senderId, artefactId, propertyName, oldValue, newValue);
-
-            PropertyUpdated message = new PropertyUpdated
-            {
-                SenderId = senderId,
-                ArtefactId = artefactId,
-                PropertyName = propertyName,
-                OldValue = oldValue,
-                NewValue = newValue
-            };
-
-            byte[] serialized = _formatter.Serialize(message);
-
-            _socketServer.Push(serialized);
+            _proxy.UpdateProperty(senderId, artefactId, propertyName, oldValue, newValue);
         }
 
         public void MoveCaret(Guid senderId, Guid artefactId, string fieldName, int caretIndex)
         {
-            _peerClient.MoveCaret(senderId, artefactId, fieldName, caretIndex);
-
-            CaretMoved message = new CaretMoved
-            {
-                SenderId = senderId,
-                ArtefactId = artefactId,
-                CaretIndex = caretIndex,
-                FieldName = fieldName
-            };
-
-            byte[] serialized = _formatter.Serialize(message);
-
-            _socketServer.Push(serialized);
+            _proxy.MoveCaret(senderId, artefactId, fieldName, caretIndex);
         }
-
-        #endregion Incoming Calls from Silverlight Clients
     }
 }
