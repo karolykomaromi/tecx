89cf08d76ea2c9a0457f7e0c0da2180372a9fce2
Damien80 <Damien80@localhost>
2011-01-14 09:13:17 +0000

    speech module with some basic commands

--- src/TecX/TecX.Agile.Moduls.Speech/Module.cs
+++ src/TecX/TecX.Agile.Moduls.Speech/Module.cs
@@ -1,17 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Windows.Threading;
 
 using Microsoft.Practices.Prism.Modularity;
+
+using TecX.Common;
 
 namespace TecX.Agile.Moduls.Speech
 {
     public class Module : IModule
     {
+        private readonly Dispatcher _dispatcher;
+        private SpeechRecognition _speechRecognition;
+
+        public Module(Dispatcher dispatcher)
+        {
+            Guard.AssertNotNull(dispatcher, "dispatcher");
+
+            _dispatcher = dispatcher;
+        }
+
         public void Initialize()
         {
-            
+            _speechRecognition = new SpeechRecognition(_dispatcher);
         }
     }
 }
