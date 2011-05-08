1727cb485697e694bcc2f11795f74d8c3bd1b51c
Damien80 <Damien80@localhost>
2011-05-08 21:25:17 +0000

    [FEATURE] websocket remote control

---
+++ src/TecX/TecX.Agile.Remote.WebSockets/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+using Microsoft.ServiceModel.WebSockets;
+
+namespace TecX.Agile.Remote.WebSockets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sh = new WebSocketsHost<MessageRelayService>(new Uri("ws://" + Environment.MachineName + ":4502/relay"));
+            sh.AddWebSocketsEndpoint();
+            sh.Open();
+
+            Console.WriteLine("Press ENTER to stop service");
+            Console.ReadLine();
+        }
+    }
+}
