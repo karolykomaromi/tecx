f0833ab9243d65292a9aa256edea622b3db97b4f
SebastianWeber <SebastianWeber@prosim3059.sv.db.de>
2011-07-20 12:28:07 +0200

    [FEATURE] basic implementation of dijkstra algorithm

---
+++ src/TecX/TecX.Dijkstra.Test/When_CalculatingShortestPathBetween6And4.cs
@@ -0,0 +1,28 @@
+using System.Collections.Generic;
+using System.Linq;
+
+using Dijkstra;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TecX.Dijkstra.Test
+{
+    [TestClass]
+    public class When_CalculatingShortestPathBetween6And4 : Given_DijkstraAndInputData
+    {
+        private List<Node> path;
+
+        protected override void When()
+        {
+            this.path = this.algorithm.CalculateDistance(this.dictNodes[6], this.dictNodes[4]).ToList();
+        }
+
+        [TestMethod]
+        public void Then_RouteMustBe6To3To4()
+        {
+            Assert.AreEqual(this.path[0], new Node(6, 0.0));
+            Assert.AreEqual(this.path[1], new Node(3, 0.0));
+            Assert.AreEqual(this.path[2], new Node(4, 0.0));
+        }
+    }
+}
---
+++ src/TecX/TecX.Dijkstra/EdgeEqualityComparer.cs
@@ -0,0 +1,17 @@
+namespace Dijkstra
+{
+    using System.Collections.Generic;
+
+    public class EdgeEqualityComparer : EqualityComparer<Edge>
+    {
+        public override bool Equals(Edge x, Edge y)
+        {
+            return x.StartNode.Id == y.StartNode.Id && x.EndNode.Id == y.EndNode.Id;
+        }
+
+        public override int GetHashCode(Edge obj)
+        {
+            return obj.Id.GetHashCode();
+        }
+    }
+}
