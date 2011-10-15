dba60542b74a556529ae98f28c978be77a6e4f71
snd\Damien80_cp <snd\Damien80_cp@localhost>
2011-10-15 08:39:20 +0200

    [FIX] added referenced assemblies from nuget

---
+++ src/TecX/TecX.Agile/Registration/AppConfigurationBuilder.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Practices.Unity;
+
+using TecX.Common.Event.Unity;
+using TecX.Unity.Configuration;
+
+namespace TecX.Agile.Registration
+{
+    public class AppConfigurationBuilder : ConfigurationBuilder
+    {
+        public AppConfigurationBuilder()
+        {
+            AddExpression(x => x.AddModification(container => container.AddNewExtension<EventAggregatorExtension>()));
+        }
+    }
+}
