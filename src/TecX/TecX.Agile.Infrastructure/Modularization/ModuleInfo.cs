089b6bcee3ae5ebf4bc4309e96515940287def63
SebastianWeber <SebastianWeber@prosim3059.sv.db.de>
2011-10-20 12:41:26 +0200

    [FEATURE] convenience ctor for ModuleInfo

--- src/TecX/TecX.Agile.Infrastructure/Modularization/ModuleInfo.cs
+++ src/TecX/TecX.Agile.Infrastructure/Modularization/ModuleInfo.cs
@@ -1,7 +1,23 @@
 namespace TecX.Agile.Infrastructure.Modularization
 {
+    using System;
+
+    using TecX.Common;
+
     public class ModuleInfo
     {
+        public ModuleInfo()
+        {
+        }
+
+        public ModuleInfo(Type moduleType)
+        {
+            Guard.AssertNotNull(moduleType, "moduleType");
+
+            Name = moduleType.Name;
+            ModuleType = moduleType.AssemblyQualifiedName;
+        }
+
         public string Name { get; set; }
 
         public string ModuleType { get; set; }
