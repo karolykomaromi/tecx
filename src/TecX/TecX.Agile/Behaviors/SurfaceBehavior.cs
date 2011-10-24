8805309ca0000477a012040c277c0761b740662f
snd\Damien80_cp <snd\Damien80_cp@localhost>
2011-10-24 17:23:31 +0200

    [FEATURE] identify surface [FEATURE] added translateonlyarea

---
+++ src/TecX/TecX.Agile/Behaviors/SurfaceBehavior.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows.Controls;
+using System.Windows.Interactivity;
+
+namespace TecX.Agile.Behaviors
+{
+    public class SurfaceBehavior : Behavior<Canvas>
+    {
+        private static Canvas _surface;
+
+        public static Canvas Surface
+        {
+            get
+            {
+                if(_surface == null)
+                {
+                    throw new InvalidOperationException("No Canvas is decorated with the SurfaceBehavior.");
+                }
+
+                return _surface;
+            }
+
+            set
+            {
+                if(_surface != null)
+                {
+                    throw new InvalidOperationException("There are multiple Canvas decorated with the SurfaceBehavior");
+                }
+
+                _surface = value;
+            }
+        }
+
+        protected override void OnAttached()
+        {
+            Surface = AssociatedObject;
+        }
+    }
+}
