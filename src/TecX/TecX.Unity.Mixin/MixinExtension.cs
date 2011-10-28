9e2d53751e08822e3dc127988af4a16dd4a692ff
snd\Damien80_cp <snd\Damien80_cp@localhost>
2011-10-28 20:45:41 +0200

    [FEATURE] unity extension that uses mixin objectfactory to add mixins to objects. SUPER SWEET!

---
+++ src/TecX/TecX.Unity.Mixin/MixinExtension.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Practices.Unity;
+using Microsoft.Practices.Unity.ObjectBuilder;
+
+namespace TecX.Unity.Mixin
+{
+    public class MixinExtension : UnityContainerExtension
+    {
+        protected override void Initialize()
+        {
+            MixinStrategy strategy = new MixinStrategy();
+
+            Context.Strategies.Add(strategy, UnityBuildStage.PreCreation);
+        }
+    }
+}
---
+++ src/TecX/TecX.Unity.Mixin/MixinObjectFactoryBuildPlanPolicy.cs
@@ -0,0 +1,35 @@
+using Microsoft.Practices.ObjectBuilder2;
+
+using Remotion.Mixins;
+using Remotion.Reflection;
+
+using TecX.Common;
+
+namespace TecX.Unity.Mixin
+{
+    public class MixinObjectFactoryBuildPlanPolicy : IBuildPlanPolicy
+    {
+        private readonly ParamList _paramList;
+
+        public MixinObjectFactoryBuildPlanPolicy(ParamList paramList)
+        {
+            Guard.AssertNotNull(paramList, "paramList");
+
+            _paramList = paramList;
+        }
+
+        public void BuildUp(IBuilderContext context)
+        {
+            Guard.AssertNotNull(context, "context");
+
+            if(context.Existing != null)
+            {
+                return;
+            }
+
+            var mixinTarget = ObjectFactory.Create(context.BuildKey.Type, _paramList);
+
+            context.Existing = mixinTarget;
+        }
+    }
+}
