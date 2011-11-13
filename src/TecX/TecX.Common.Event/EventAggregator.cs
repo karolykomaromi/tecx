a6bd5d54122d887f87eebc63ea429d1e026f5fda
WeberSe <WeberSe@WKWEBERSE04.frankfurt.sdx-ag.de>
2011-11-13 19:03:23 +0100

    [CLEAN] tecx.common.event stylecop warning free

--- src/TecX/TecX.Common.Event/EventAggregator.cs
+++ src/TecX/TecX.Common.Event/EventAggregator.cs
@@ -1,26 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Windows.Threading;
-
-namespace TecX.Common.Event
+﻿namespace TecX.Common.Event
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Windows.Threading;
+
     public class EventAggregator : IEventAggregator
     {
-        #region Fields
-
         // list to store all subscribers for ALL messages
-        private List<WeakReference> _subscribers = new List<WeakReference>();
+        private readonly List<WeakReference> subscribers = new List<WeakReference>();
 
         // A Dispatcher for marshalling calls to the UI-Thread (update databound lists, etc.)
-        private Dispatcher _dispatcher;
+        private readonly Dispatcher dispatcher;
 
         // Thread-Synchronization locking object for access to Subscribers list
-        private object _lock = new object();
+        private readonly object locker = new object();
 
-        #endregion Fields
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EventAggregator"/> class.
+        /// </summary>
+        /// <param name="dispatcher"> Dispatcher. Comes from App.xaml.cs</param>
+        public EventAggregator(Dispatcher dispatcher)
+        {
+            Guard.AssertNotNull(dispatcher, "dispatcher");
 
-        #region Properties
+            this.dispatcher = dispatcher;
+
+            // just one rough  possibility to purge dead subscriptions
+            // runs every minute - needs to be adjusted for other apps.
+#if SILVERLIGHT
+            var dispatcherTimer = new DispatcherTimer();
+#else
+            var dispatcherTimer = new DispatcherTimer(DispatcherPriority.SystemIdle);
+#endif
+            dispatcherTimer.Tick += this.RemoveDeadReferences;
+            dispatcherTimer.Interval = TimeSpan.FromMinutes(1);
+            dispatcherTimer.Start();
+        }
 
         /// <summary>
         /// Gets the subscribers. For UnitTesting.
@@ -30,39 +46,9 @@ namespace TecX.Common.Event
         {
             get
             {
-                return _subscribers;
+                return this.subscribers;
             }
         }
-
-        #endregion Properties
-
-        #region c'tor
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="EventAggregator"/> class.
-        /// </summary>
-        /// <param name="dispatcher">Dispatcher. Comes from App.xaml.cs</param>
-        public EventAggregator(Dispatcher dispatcher)
-        {
-            Guard.AssertNotNull(dispatcher, "dispatcher");
-
-            _dispatcher = dispatcher;
-
-            // just one rough  possibility to purge dead subscriptions
-            // runs every minute - needs to be adjusted for other apps.
-#if SILVERLIGHT
-            var dispatcherTimer = new DispatcherTimer();
-#else
-            var dispatcherTimer = new DispatcherTimer(DispatcherPriority.SystemIdle);
-#endif
-            dispatcherTimer.Tick += RemoveDeadReferences;
-            dispatcherTimer.Interval = TimeSpan.FromMinutes(1);
-            dispatcherTimer.Start();
-        }
-
-        #endregion c'tor
-
-        #region Implementation of IEventAggregator
 
         /// <summary>
         /// Subscribes an object to the event aggregator.
@@ -72,14 +58,16 @@ namespace TecX.Common.Event
         {
             Guard.AssertNotNull(subscriber, "subscriber");
 
-            RunLocked(() =>
+            this.RunLocked(() =>
             {
                 if (IsAlreadyInCollection(subscriber))
+                {
                     return;
+                }
 
                 AssertHandlerInterfaceImplemented(subscriber);
 
-                _subscribers.Add(new WeakReference(subscriber));
+                this.subscribers.Add(new WeakReference(subscriber));
             });
         }
 
@@ -91,14 +79,14 @@ namespace TecX.Common.Event
         {
             Guard.AssertNotNull(subscriber, "subscriber");
 
-            RunLocked(() =>
+            this.RunLocked(() =>
             {
-                var weakRefToRemove = (from item in _subscribers
+                var weakRefToRemove = (from item in this.subscribers
                                        where item.Target == subscriber
                                        select item)
                         .FirstOrDefault();
 
-                _subscribers.Remove(weakRefToRemove);
+                this.subscribers.Remove(weakRefToRemove);
             });
         }
 
@@ -113,16 +101,16 @@ namespace TecX.Common.Event
         {
             Guard.AssertNotNull(message, "message");
 
-            RunLocked(() =>
+            this.RunLocked(() =>
             {
                 foreach (var subscriber in GetAllSubscribersFor<TMessage>())
                 {
-                    if (_dispatcher != null)
+                    if (this.dispatcher != null)
                     {
 #if SILVERLIGHT
                         _dispatcher.BeginInvoke(new Action(() => subscriber.Handle(message)));
 #else
-                        _dispatcher.Invoke(DispatcherPriority.Normal, new Action(() => subscriber.Handle(message)));
+                        this.dispatcher.Invoke(DispatcherPriority.Normal, new Action(() => subscriber.Handle(message)));
 #endif
                     }
                     else
@@ -135,23 +123,6 @@ namespace TecX.Common.Event
             return message;
         }
 
-        #endregion Implementation of IEventAggregator
-
-        #region Helper
-
-        /// <summary>
-        /// Gets all subscribers for a specific messagetype.
-        /// </summary>
-        /// <typeparam name="TMessage">The messagetype to search registered implementers for.</typeparam>
-        /// <returns>List of all subscribers for <typeparamref name="TMessage"/></returns>
-        private IEnumerable<ISubscribeTo<TMessage>> GetAllSubscribersFor<TMessage>()
-        {
-            return (from subscriber in _subscribers
-                    let handler = subscriber.Target as ISubscribeTo<TMessage>
-                    where handler != null
-                    select handler).ToList();
-        }
-
         /// <summary>
         /// This is just to find wether an object subscribing is implementing the open generic version of IHandle<> 
         /// with any TMessage. It does not matter which TMessage exactly.
@@ -162,8 +133,8 @@ namespace TecX.Common.Event
         /// </returns>
         private static void AssertHandlerInterfaceImplemented(object subscriber)
         {
-            Type[] interfaces = subscriber.GetType()
-                    .FindInterfaces((type, criteria) =>
+            Type[] interfaces = subscriber.GetType().FindInterfaces(
+                (type, criteria) =>
                     {
                         if (type.IsGenericType &&
                             type.GetGenericTypeDefinition() == typeof(ISubscribeTo<>))
@@ -172,7 +143,7 @@ namespace TecX.Common.Event
                         }
 
                         return false;
-                    }, 
+                    },
                     null);
 
             if (interfaces.Length == 0)
@@ -182,13 +153,26 @@ namespace TecX.Common.Event
         }
 
         /// <summary>
+        /// Gets all subscribers for a specific messagetype.
+        /// </summary>
+        /// <typeparam name="TMessage">The messagetype to search registered implementers for.</typeparam>
+        /// <returns>List of all subscribers for <typeparamref name="TMessage"/></returns>
+        private IEnumerable<ISubscribeTo<TMessage>> GetAllSubscribersFor<TMessage>()
+        {
+            return (from subscriber in this.subscribers
+                    let handler = subscriber.Target as ISubscribeTo<TMessage>
+                    where handler != null
+                    select handler).ToList();
+        }
+
+        /// <summary>
         /// Runs all actions within a lock for thread-safe access to the subscribers list.
         /// ReaderWriterLock is a better choice than simple locking but it is more complex. 
         /// </summary>
         /// <param name="action">The action to execute within a lock.</param>
         private void RunLocked(Action action)
         {
-            lock (_lock)
+            lock (this.locker)
             {
                 action.Invoke();
             }
@@ -203,7 +187,7 @@ namespace TecX.Common.Event
         /// </returns>
         private bool IsAlreadyInCollection(object subscriber)
         {
-            var element = _subscribers.Select(o => o).Where(o => o.Target == subscriber).FirstOrDefault();
+            var element = this.subscribers.Select(o => o).Where(o => o.Target == subscriber).FirstOrDefault();
 
             return element != null;
         }
@@ -213,16 +197,14 @@ namespace TecX.Common.Event
         /// </summary>
         private void RemoveDeadReferences(object sender, EventArgs eventArgs)
         {
-            RunLocked(() =>
+            this.RunLocked(() =>
             {
-                var weakReferences = (from item in _subscribers
+                var weakReferences = (from item in this.subscribers
                                       where !item.IsAlive
                                       select item).ToList();
 
-                weakReferences.ForEach(reference => _subscribers.Remove(reference));
+                weakReferences.ForEach(reference => this.subscribers.Remove(reference));
             });
         }
-
-        #endregion Helper
     }
 }
