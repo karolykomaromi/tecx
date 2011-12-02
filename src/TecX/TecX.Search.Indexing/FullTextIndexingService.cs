1a01eb7949d7179dfbca6f40fc66fa39ec1194c6
snd\Damien80_cp <snd\Damien80_cp@localhost>
2011-12-02 17:24:19 +0100

    [FEATURE] indexing service for full-text search

---
+++ src/TecX/TecX.Search.Indexing/FullTextIndexingService.cs
@@ -0,0 +1,33 @@
+﻿namespace TecX.Search.Indexing
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Timers;
+
+    public class FullTextIndexingService
+    {
+        private readonly Timer timer;
+
+        public FullTextIndexingService(TimeSpan indexingIntervall)
+        {
+            this.timer = new Timer(indexingIntervall.TotalSeconds);
+            this.timer.Elapsed += this.OnElapsed;
+        }
+
+        public void Start()
+        {
+            this.timer.Start();
+        }
+
+        public void Stop()
+        {
+            this.timer.Stop();
+        }
+
+        private void OnElapsed(object sender, ElapsedEventArgs e)
+        {
+        }
+    }
+}
