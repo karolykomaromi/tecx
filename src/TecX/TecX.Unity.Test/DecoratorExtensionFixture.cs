b97049536d1361c3e90a87edef859251e5d5dc46
snd\Damien80_cp <snd\Damien80_cp@localhost>
2012-01-05 14:27:53 +0100

    [FEATURE] nice extension for unity that allows application of the decorator pattern and drastically reduces noise during registration

---
+++ src/TecX/TecX.Unity.Test/DecoratorExtensionFixture.cs
@@ -0,0 +1,41 @@
+﻿namespace TecX.Unity.Test
+{
+    using Microsoft.Practices.Unity;
+
+    using TecX.Common.Extensions.Primitives;
+    using TecX.Unity.Decoration;
+    using TecX.Unity.Test.TestObjects;
+
+    using Xunit;
+
+    public class DecoratorExtensionFixture
+    {
+        [Fact]
+        public void CreatesTheMostDependentType()
+        {
+            var container = new UnityContainer()
+                .AddExtension(new DecoratorExtension())
+                .RegisterType<IContract, ContractDecorator>()
+                .RegisterType<IContract, Contract>("1");
+
+            var sut = container.Resolve<IContract>();
+            Assert.NotNull(sut);
+            Assert.IsType(typeof(ContractDecorator), sut);
+            Assert.IsType(typeof(Contract), ((ContractDecorator)sut).Base);
+        }
+
+        [Fact]
+        public void UsesSpecialRegistrationFeatures()
+        {
+            var container = new UnityContainer()
+                .AddNewExtension<DecoratorExtension>()
+                .RegisterType<IContract, ContractDecorator>(new InjectionProperty("Bar", "2"))
+                .RegisterType<IContract, Contract>(new InjectionProperty("Foo", "1"));
+
+            var sut = container.Resolve<IContract>();
+
+            Assert.Equal("2", sut.To<ContractDecorator>().Bar);
+            Assert.Equal("1", sut.To<ContractDecorator>().Base.To<Contract>().Foo);
+        }
+    }
+}
---
+++ src/TecX/TecX.Unity/Decoration/DecoratorExtension.cs
@@ -0,0 +1,33 @@
+﻿namespace TecX.Unity.Decoration
+{
+    using System;
+    using System.Collections.Generic;
+
+    using Microsoft.Practices.Unity;
+    using Microsoft.Practices.Unity.ObjectBuilder;
+
+    public class DecoratorExtension : UnityContainerExtension
+    {
+        private RegistrationStack registrationStack;
+
+        protected override void Initialize()
+        {
+            this.registrationStack = new RegistrationStack();
+            this.Context.Registering += AddRegistration;
+
+            this.Context.Strategies.Add(new DecoratorStrategy(this.registrationStack), UnityBuildStage.PreCreation);
+        }
+
+        private void AddRegistration(object sender, RegisterEventArgs e)
+        {
+            if (!e.TypeFrom.IsInterface)
+            {
+                return;
+            }
+
+            ICollection<Type> stack = this.registrationStack[e.TypeFrom];
+
+            stack.Add(e.TypeTo);
+        }
+    }
+}
