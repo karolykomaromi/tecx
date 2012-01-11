54bd4b0ef962026e40e3ec543102db5f1956017d
snd\Damien80_cp <snd\Damien80_cp@localhost>
2012-01-11 22:49:19 +0100

    [FEATURE] added phone project with code from caliburn.micro sample

---
+++ src/TecX/TecX.Agile.Phone/TabViewModelStorage.cs
@@ -0,0 +1,16 @@
+﻿namespace TecX.Agile.Phone
+{
+    using Caliburn.Micro;
+
+    public class TabViewModelStorage : StorageHandler<TabViewModel>
+    {
+        public override void Configure()
+        {
+            Id(x => x.DisplayName);
+
+            Property(x => x.Text)
+                .InPhoneState()
+                .RestoreAfterActivation();
+        }
+    }
+}
