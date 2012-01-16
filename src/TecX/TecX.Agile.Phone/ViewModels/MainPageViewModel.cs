76c32486594e6ac445f0f1c9ff7f7078de5531a8
snd\Damien80_cp <snd\Damien80_cp@localhost>
2012-01-16 23:23:58 +0100

    [FEATURE] calling wcf service from phone app using less ugly custom client

--- src/TecX/TecX.Agile.Phone/ViewModels/MainPageViewModel.cs
+++ src/TecX/TecX.Agile.Phone/ViewModels/MainPageViewModel.cs
@@ -1,21 +1,41 @@
 ﻿namespace TecX.Agile.Phone.ViewModels
 {
+    using System.Diagnostics;
+
     using Caliburn.Micro;
+
+    using TecX.Agile.Phone.Service;
 
     public class MainPageViewModel
     {
-        readonly INavigationService navigationService;
+        private readonly INavigationService navigationService;
 
-        public MainPageViewModel(INavigationService navigationService)
+        private readonly IAsyncProjectService projectService;
+
+        public MainPageViewModel(INavigationService navigationService, IAsyncProjectService projectService)
         {
+            Guard.AssertNotNull(navigationService, "navigationService");
+            Guard.AssertNotNull(projectService, "projectService");
+
             this.navigationService = navigationService;
+            this.projectService = projectService;
         }
 
         public void GotoPageTwo()
         {
-            this.navigationService.UriFor<PivotPageViewModel>()
-                .WithParam(x => x.NumberOfTabs, 5)
-                .Navigate();
+            //this.navigationService.UriFor<PivotPageViewModel>()
+            //    .WithParam(x => x.NumberOfTabs, 5)
+            //    .Navigate();
+
+            var x = this.projectService.BeginGetProjects(
+                100,
+                asyncResult =>
+                    {
+                        var svc = (IAsyncProjectService)asyncResult.AsyncState;
+                        var projectQueryResult = svc.EndGetProjects(asyncResult);
+                        Debug.WriteLine("call successfull");
+                    },
+                this.projectService);
         }
     }
 }
