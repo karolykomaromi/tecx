d188ef7781a0d4de81b01ce5ac4f709d2c4b08b3
webeseb <webeseb@NEMO02.netdev.deutsche-boerse.de>
2012-02-13 16:57:21 +0100

    [FEATURE] enabled contextual binding via injection member. now this is AWESOME!

---
+++ src/TecX/TecX.Unity.ContextualBinding/ContextualBinding.cs
@@ -0,0 +1,41 @@
+﻿namespace TecX.Unity.ContextualBinding
+{
+    using System;
+
+    using Microsoft.Practices.ObjectBuilder2;
+    using Microsoft.Practices.Unity;
+
+    using TecX.Common;
+
+    public class ContextualBinding : InjectionMember
+    {
+        private readonly Predicate<IBindingContext, IBuilderContext> predicate;
+
+        public ContextualBinding(Predicate<IBindingContext, IBuilderContext> predicate)
+        {
+            Guard.AssertNotNull(predicate, "predicate");
+
+            this.predicate = predicate;
+        }
+
+        public override void AddPolicies(Type serviceType, Type implementationType, string name, IPolicyList policies)
+        {
+            Guard.AssertNotNull(policies, "policies");
+
+            var context = policies.Get<IContextPolicy>(Constants.ContextPolicyKey);
+
+            if (context == null)
+            {
+                throw new InvalidOperationException("Can't find IContextPolicy. " +
+                    "Did you forget to add the ContextualBindingExtension to your container instance?");
+            }
+
+            context.AddContextualMapping(serviceType, implementationType, this.predicate);
+        }
+    }
+
+    public interface IContextPolicy : IBuilderPolicy
+    {
+        void AddContextualMapping(Type @from, Type to, Predicate<IBindingContext, IBuilderContext> predicate);
+    }
+}
