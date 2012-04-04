6625e5a7501c73e427c459b6815567a2474b2f4c
webeseb <webeseb@NEMO02.netdev.deutsche-boerse.de>
2012-04-04 13:32:45 +0200

    [FEATURE] convention to wire up decorated command handlers

--- src/TecX/TecX.Unity/Decoration/RegistrationStack.cs
+++ src/TecX/TecX.Unity/Decoration/RegistrationStack.cs
@@ -3,39 +3,41 @@ namespace TecX.Unity.Decoration
     using System;
     using System.Collections.Generic;
 
+    using Microsoft.Practices.ObjectBuilder2;
+
     using TecX.Common;
 
     public class RegistrationStack
     {
-        private readonly Dictionary<Type, ICollection<Type>> registrationStack;
+        private readonly Dictionary<NamedTypeBuildKey, ICollection<Type>> registrationStack;
 
         public RegistrationStack()
         {
-            this.registrationStack = new Dictionary<Type, ICollection<Type>>();
+            this.registrationStack = new Dictionary<NamedTypeBuildKey, ICollection<Type>>();
         }
 
-        public ICollection<Type> this[Type contract]
+        public ICollection<Type> this[NamedTypeBuildKey key]
         {
             get
             {
-                Guard.AssertNotNull(contract, "contract");
+                Guard.AssertNotNull(key, "key");
 
                 ICollection<Type> stack;
-                if (!this.registrationStack.TryGetValue(contract, out stack))
+                if (!this.registrationStack.TryGetValue(key, out stack))
                 {
                     stack = new List<Type>();
-                    this.registrationStack[contract] = stack;
+                    this.registrationStack[key] = stack;
                 }
 
                 return stack;
             }
         }
 
-        public bool IsRegistered(Type contract)
+        public bool IsRegistered(NamedTypeBuildKey key)
         {
-            Guard.AssertNotNull(contract, "contract");
+            Guard.AssertNotNull(key, "key");
 
-            return this.registrationStack.ContainsKey(contract);
+            return this.registrationStack.ContainsKey(key);
         }
     }
 }
