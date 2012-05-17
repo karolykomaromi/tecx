44d15e81c71ec7569c2d2979508ff74f129a45df
snd\Damien80_cp <snd\Damien80_cp@localhost>
2012-05-17 19:29:04 +0200

    [FEATURE] auto-mocking with unity and moq

---
+++ src/TecX/TecX.TestTools.Test/AutoMockingExtensionFixture.cs
@@ -0,0 +1,38 @@
+﻿namespace TecX.TestTools.Test
+{
+    using Microsoft.Practices.Unity;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using TecX.TestTools.Test.TestObjects;
+    using TecX.TestTools.Unity;
+
+    [TestClass]
+    public class AutoMockingExtensionFixture
+    {
+        [TestMethod]
+        public void CanTurnOnAutoMocking()
+        {
+            var container = new UnityContainer();
+
+            container.AddNewExtension<AutoMockingExtension>();
+
+            var foo = container.Resolve<IFoo>();
+
+            Assert.IsNotNull(foo);
+        }
+
+        [TestMethod]
+        public void CanConfigureMock()
+        {
+            var container = new UnityContainer();
+
+            var mock = container.Mock<IFoo>();
+
+            mock.SetupGet(f => f.Bar).Returns("1");
+
+            IFoo foo = container.Resolve<IFoo>();
+
+            Assert.AreEqual("1", foo.Bar);
+        }
+    }
+}
