612dcba756ae32c3fccd46209e68b01a3a4e2cbe
snd\Damien80_cp <snd\Damien80_cp@localhost>
2012-05-17 19:29:04 +0200

    [FEATURE] auto-mocking with unity and moq

---
+++ src/TecX/TecX.TestTools/Unity/AutoMockingExtension.cs
@@ -0,0 +1,15 @@
+﻿namespace TecX.TestTools.Unity
+{
+    using Microsoft.Practices.Unity;
+    using Microsoft.Practices.Unity.ObjectBuilder;
+
+    public class AutoMockingExtension : UnityContainerExtension
+    {
+        protected override void Initialize()
+        {
+            var strategy = new AutoMockingBuilderStrategy(this.Container);
+
+            this.Context.Strategies.Add(strategy, UnityBuildStage.PreCreation);
+        }
+    }
+}
