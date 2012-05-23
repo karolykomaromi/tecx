d69c928495d521be5343600355b2b6bdf22c53b1
weberse <weberse@localhost>
2012-05-23 14:35:41 +0200

    [FEATURE] integrate unity with web projects

---
+++ src/TecX/TecX.Web.Unity/CompositionRoot.cs
@@ -0,0 +1,34 @@
+﻿namespace TecX.Web.Unity
+{
+    using System;
+    using System.Threading.Tasks;
+    using System.Web.Http.Controllers;
+    using System.Web.Http.Dispatcher;
+
+    using Microsoft.Practices.Unity;
+
+    using TecX.Unity.Decoration;
+    using TecX.Unity.Factories;
+
+    class CompositionRoot
+    {
+        public CompositionRoot()
+        {
+            var container = new UnityContainer();
+
+            container.AddNewExtension<DecoratorExtension>();
+
+            container.RegisterType<IHttpControllerActivator, ContextCapturingControllerActivator>(new ContainerControlledLifetimeManager());
+
+            container.RegisterType<IHttpControllerActivator, DefaultHttpControllerActivator>(new ContainerControlledLifetimeManager());
+
+            container.RegisterType<Func<TaskCompletionSource<HttpControllerContext>>>(new ContainerControlledLifetimeManager(), new DelegateFactory());
+
+            container.RegisterType<TaskCompletionSource<HttpControllerContext>>(new PerWebRequestLifetimeManager());
+
+            container.RegisterType<HttpControllerContext>(
+                new PerWebRequestLifetimeManager(),
+                new InjectionFactory(c => c.Resolve<TaskCompletionSource<HttpControllerContext>>().Task.Result));
+        }
+    }
+}
