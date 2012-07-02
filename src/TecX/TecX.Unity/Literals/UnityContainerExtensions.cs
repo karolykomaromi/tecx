7d6282e25048058ab0d8398af364847d390578d3
snd\Damien80_cp <snd\Damien80_cp@localhost>
2012-07-02 21:10:30 +0200

    [UPDATE] extension method for adding single conventions

--- src/TecX/TecX.Unity/Literals/UnityContainerExtensions.cs
+++ src/TecX/TecX.Unity/Literals/UnityContainerExtensions.cs
@@ -26,5 +26,26 @@
 
             return container;
         }
+
+        public static IUnityContainer WithConventionForLiteralParameters(this IUnityContainer container, IDependencyResolverConvention convention)
+        {
+            Guard.AssertNotNull(container, "container");
+            Guard.AssertNotNull(convention, "convention");
+
+            ILiteralParameters parameters = container.Configure<ILiteralParameters>();
+
+            if (parameters == null)
+            {
+                var extension = new LiteralParametersExtension();
+
+                container.AddExtension(extension);
+
+                parameters = extension;
+            }
+
+            parameters.AddConvention(convention);
+
+            return container;
+        }
     }
 }
