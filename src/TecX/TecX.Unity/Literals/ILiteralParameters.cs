8e6864fb4a0883bdb3840b4c0352164839226f76
weberse <weberse@localhost>
2012-07-30 09:49:40 +0200

    [CLEAN] removed obsolete interface [UPDATE] tracetimer can find out calling method using reflection

--- src/TecX/TecX.Unity/Literals/ILiteralParameters.cs
+++
@@ -1,9 +0,0 @@
-namespace TecX.Unity.Literals
-{
-    using Microsoft.Practices.Unity;
-
-    public interface ILiteralParameters : IUnityContainerExtensionConfigurator
-    {
-        void AddConvention(IDependencyResolverConvention convention);
-    }
-}
