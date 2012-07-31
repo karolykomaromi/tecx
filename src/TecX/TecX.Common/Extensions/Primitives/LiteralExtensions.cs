2386d0c116b7564343ba71e5a705b254ccb85fb6
weberse <weberse@localhost>
2012-07-31 13:02:02 +0200

    [CLEAN] hide from debugger

--- src/TecX/TecX.Common/Extensions/Primitives/LiteralExtensions.cs
+++ src/TecX/TecX.Common/Extensions/Primitives/LiteralExtensions.cs
@@ -1,54 +1,65 @@
 ﻿namespace TecX.Common.Extensions.Primitives
 {
     using System;
+    using System.Diagnostics;
 
     public static class LiteralExtensions
     {
+        [DebuggerStepThrough]
         public static TimeSpan Milliseconds(this int ts)
         {
             return TimeSpan.FromMilliseconds(ts);
         }
 
+        [DebuggerStepThrough]
         public static TimeSpan Seconds(this int ts)
         {
             return TimeSpan.FromSeconds(ts);
         }
 
+        [DebuggerStepThrough]
         public static TimeSpan Minutes(this int ts)
         {
             return TimeSpan.FromMinutes(ts);
         }
 
+        [DebuggerStepThrough]
         public static TimeSpan Hours(this int ts)
         {
             return TimeSpan.FromHours(ts);
         }
 
+        [DebuggerStepThrough]
         public static TimeSpan Days(this int ts)
         {
             return TimeSpan.FromDays(ts);
         }
 
+        [DebuggerStepThrough]
         public static TimeSpan Milliseconds(this double ts)
         {
             return TimeSpan.FromMilliseconds(ts);
         }
 
+        [DebuggerStepThrough]
         public static TimeSpan Seconds(this double ts)
         {
             return TimeSpan.FromSeconds(ts);
         }
 
+        [DebuggerStepThrough]
         public static TimeSpan Minutes(this double ts)
         {
             return TimeSpan.FromMinutes(ts);
         }
 
+        [DebuggerStepThrough]
         public static TimeSpan Hours(this double ts)
         {
             return TimeSpan.FromHours(ts);
         }
 
+        [DebuggerStepThrough]
         public static TimeSpan Days(this double ts)
         {
             return TimeSpan.FromDays(ts);
