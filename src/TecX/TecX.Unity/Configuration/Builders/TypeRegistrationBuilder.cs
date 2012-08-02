166c34b568db5c360001cc09bf503b4d8f8bb4e8
snd\Damien80_cp <snd\Damien80_cp@localhost>
2012-08-02 21:59:34 +0200

    [CLEAN] made anonymous override injectionmember and put it into appropriate folder

--- src/TecX/TecX.Unity/Configuration/Builders/TypeRegistrationBuilder.cs
+++ src/TecX/TecX.Unity/Configuration/Builders/TypeRegistrationBuilder.cs
@@ -160,7 +160,7 @@ namespace TecX.Unity.Configuration.Builders
         {
             Guard.AssertNotNull(anonymous, "anonymous");
 
-            var overrides = new AnonymousTypeOverrideSpec(anonymous, this.To);
+            var overrides = new AnonymousParameterOverride(anonymous, this.To);
 
             this.enrichments.Add(overrides);
 
