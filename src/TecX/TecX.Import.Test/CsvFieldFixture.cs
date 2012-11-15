c0dd684e9f4718c8575228a1a36964f2fc119699
snd\Damien80_cp <snd\Damien80_cp@localhost>
2012-11-15 21:40:44 +0100

    [FEATURE] built a simple csv reader

---
+++ src/TecX/TecX.Import.Test/CsvFieldFixture.cs
@@ -0,0 +1,24 @@
+﻿namespace TecX.Import.Test
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class CsvFieldFixture
+    {
+        [TestMethod]
+        public void Should_MismatchNullAndField()
+        {
+            var empty = CsvField.Empty;
+
+            Assert.IsFalse(empty == null);
+        }
+
+        [TestMethod]
+        public void Should_Match_EmptyAndFieldWithSameValues()
+        {
+            var empty = new CsvField(-1, string.Empty, string.Empty);
+
+            Assert.IsTrue(empty.Equals(CsvField.Empty));
+        }
+    }
+}
