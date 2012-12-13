7be9436f4800418dd454ab7942f6014cee7db56f
snd\Damien80_cp <snd\Damien80_cp@localhost>
2012-12-13 22:09:37 +0100

    [CLEAN] kicked out all nuget packages and enabled download during build [FEATURE] small database helper class that lets you create dbs for tests

---
+++ src/TecX/TecX.TestTools/Data/Database.cs
@@ -0,0 +1,20 @@
+﻿namespace TecX.TestTools.Data
+{
+    using System;
+
+    using TecX.Common;
+
+    public static class Database
+    {
+        public static DatabaseBuildResult Build(Action<DatabaseBuildConfiguration> action)
+        {
+            Guard.AssertNotNull(action, "action");
+
+            var config = new DatabaseBuildConfiguration();
+
+            action(config);
+
+            return config.Execute();
+        }
+    }
+}
