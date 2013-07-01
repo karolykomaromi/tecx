66a7774f8a831835403fd40acf89012299ee80b9
snd\Damien80_cp <snd\Damien80_cp@localhost>
2013-07-01 22:13:43 +0200

    [FEATURE] container data attribute for xunit data theories with autofixture

---
+++ src/TecX/TecX.BehavioralTesting/BehavioralTestingFixture.cs
@@ -0,0 +1,23 @@
+﻿namespace TecX.BehavioralTesting
+{
+    using Microsoft.Practices.Unity;
+
+    using Xunit;
+    using Xunit.Extensions;
+
+    public class BehavioralTestingFixture
+    {
+        [Theory, ContainerTheory]
+        public void ShouldUseContainerToResolveParameter(IFoo foo)
+        {
+            Assert.NotNull(foo);
+        }
+
+        [Theory, ContainerTheory]
+        public void ShouldInjectChildContainer(IUnityContainer container)
+        {
+            Assert.NotNull(container);
+            Assert.NotNull(container.Parent);
+        }
+    }
+}
---
+++ src/TecX/TecX.BehavioralTesting/ContainerConfiguration.cs
@@ -0,0 +1,12 @@
+﻿namespace TecX.BehavioralTesting
+{
+    using Microsoft.Practices.Unity;
+
+    public class ContainerConfiguration : UnityContainerExtension
+    {
+        protected override void Initialize()
+        {
+            this.Container.RegisterType<IFoo, Foo>();
+        }
+    }
+}
