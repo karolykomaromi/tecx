d357f0568af26ce198ee9a708b9f3f10fcafaa4e
snd\Damien80_cp <snd\Damien80_cp@localhost>
2013-07-15 22:46:16 +0200

    [UPDATE] nhibernate global filters do just the same as rewriting the whole expression tree would...

---
+++ src/TecX/TecX.Query.Test/Hibernate/PersistentObjectMap.cs
@@ -0,0 +1,16 @@
+﻿namespace TecX.Query.Test.Hibernate
+{
+    using FluentNHibernate.Mapping;
+
+    using TecX.Query.PD;
+
+    public abstract class PersistentObjectMap<T> : ClassMap<T>
+        where T : PersistentObject
+    {
+        protected PersistentObjectMap()
+        {
+            this.Id(x => x.PDO_ID);
+            this.Map(x => x.PDO_DELETED);
+        }
+    }
+}
