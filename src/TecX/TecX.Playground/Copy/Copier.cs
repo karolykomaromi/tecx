4f742137367e70312da6ae21b83d98e1aa433d03
weberse <weberse@localhost>
2013-07-30 16:35:23 +0200

    [CLEAN] put copier stuff in separate folder

---
+++ src/TecX/TecX.Playground/Copy/Copier.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace TecX.Playground.Copy
+{
+    public class Copier
+    {
+        private readonly IEnumerable<ICopyStrategy> copyStrategies;
+
+        public Copier(IEnumerable<ICopyStrategy> copyStrategies)
+        {
+            this.copyStrategies = copyStrategies;
+        }
+
+        public T Copy<T>(T original) where T : A, new()
+        {
+            T copy = new T();
+
+            CopyContext ctx = new CopyContext(this);
+
+            var strategies = this.copyStrategies.OfType<ICopyStrategy<T>>();
+
+            foreach (ICopyStrategy<T> strategy in strategies)
+            {
+                strategy.CopyValues(original, copy, ctx);
+            }
+
+            return copy;
+        }
+    }
+}
---
+++ src/TecX/TecX.Playground/Copy/CopierFixture.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+using Xunit;
+
+namespace TecX.Playground.Copy
+{
+    public class CopierFixture
+    {
+        [Fact]
+        public void CanCopyAllHierarchyLevels()
+        {
+            C original = new C { Foo = "Foo", Bar = -1, Baz = "Baz" };
+
+            Copier copier = new Copier(CopyStrategies.All);
+
+            C copy = copier.Copy(original);
+
+            Assert.Equal(original.Foo, copy.Foo);
+            Assert.Equal(original.Bar, copy.Bar);
+            Assert.Equal(original.Baz, copy.Baz);
+        }
+
+        [Fact]
+        public void CanSkipHierarchyLevels()
+        {
+            C original = new C { Foo = "Foo", Bar = -1, Baz = "Baz" };
+
+            Copier copier = new Copier(CopyStrategies.All.Where(strategy => !string.Equals(strategy.GetType().Name, "BCopyStrategy", StringComparison.OrdinalIgnoreCase)));
+
+            C copy = copier.Copy(original);
+
+            Assert.Equal(original.Foo, copy.Foo);
+            Assert.Equal(default(int), copy.Bar);
+            Assert.Equal(original.Baz, copy.Baz);
+        }
+    }
+}
