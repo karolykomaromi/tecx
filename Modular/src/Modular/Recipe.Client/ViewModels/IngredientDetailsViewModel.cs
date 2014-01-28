7c0d1d980db6a3a0634161783d4d67a6ee97d27b
weberse <weberse@localhost>
2014-01-28 12:12:24 +0100

    [FEATURE] starting on details views for recipe and ingredients

---
+++ Modular/src/Modular/Recipe.Client/ViewModels/IngredientDetailsViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Recipe.ViewModels
+{
+    using Infrastructure.I18n;
+    using Infrastructure.ViewModels;
+
+    public class IngredientDetailsViewModel : TitledViewModel
+    {
+        public IngredientDetailsViewModel(ResxKey titleKey)
+            : base(titleKey)
+        {
+        }
+    }
+}
