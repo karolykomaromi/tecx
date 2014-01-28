a65c63f471925e5d9e21f6a78c61f3457ce64e8a
weberse <weberse@localhost>
2014-01-28 09:26:06 +0100

    [CLEAN] custom triggeraction that specifically handles autocompletebox

--- Modular/src/Modular/Infrastructure.Client/Triggers/AutoCompleteToCommand.cs
+++ Modular/src/Modular/Infrastructure.Client/Triggers/AutoCompleteToCommand.cs
@@ -1,19 +1,86 @@
 ﻿namespace Infrastructure.Triggers
 {
     using System;
+    using System.Windows;
     using System.Windows.Controls;
-    using GalaSoft.MvvmLight.Command;
+    using System.Windows.Input;
+    using System.Windows.Interactivity;
 
-    public class AutoCompleteToCommand : EventToCommand
+    public class AutoCompleteToCommand : TriggerAction<AutoCompleteBox>
     {
-        protected override void OnAttached()
+        public static readonly DependencyProperty CommandParameterProperty = DependencyProperty.Register(
+            "CommandParameter",
+            typeof(object),
+            typeof(AutoCompleteToCommand),
+            new PropertyMetadata(null, OnCommandParameterChanged));
+
+        public static readonly DependencyProperty CommandProperty = DependencyProperty.Register(
+            "Command",
+            typeof(ICommand),
+            typeof(AutoCompleteToCommand),
+            new PropertyMetadata(
+                null,
+                (s, e) => OnCommandChanged(s as AutoCompleteToCommand, e)));
+
+        public static readonly DependencyProperty MustToggleIsEnabledProperty = DependencyProperty.Register(
+            "MustToggleIsEnabled",
+            typeof(bool),
+            typeof(AutoCompleteToCommand),
+            new PropertyMetadata(
+                false,
+                OnMustToggleIsEnabledChanged));
+
+        private object commandParameterValue;
+
+        private bool? mustToggleValue;
+
+        public ICommand Command
         {
-            if (this.AssociatedObject as AutoCompleteBox == null)
+            get { return (ICommand)this.GetValue(CommandProperty); }
+
+            set { this.SetValue(CommandProperty, value); }
+        }
+
+        public object CommandParameter
+        {
+            get { return this.GetValue(CommandParameterProperty); }
+
+            set { this.SetValue(CommandParameterProperty, value); }
+        }
+
+        public object CommandParameterValue
+        {
+            get
             {
-                throw new InvalidOperationException("Sorry, I derived from EventToCommand because I was too lazy. You cannot attach to anything but AutoCompleteBox.");
+                return this.commandParameterValue ?? this.CommandParameter;
             }
 
-            base.OnAttached();
+            set
+            {
+                this.commandParameterValue = value;
+                this.EnableDisableElement();
+            }
+        }
+
+        public bool MustToggleIsEnabled
+        {
+            get { return (bool)this.GetValue(MustToggleIsEnabledProperty); }
+
+            set { this.SetValue(MustToggleIsEnabledProperty, value); }
+        }
+
+        public bool MustToggleIsEnabledValue
+        {
+            get
+            {
+                return this.mustToggleValue == null ? this.MustToggleIsEnabled : this.mustToggleValue.Value;
+            }
+
+            set
+            {
+                this.mustToggleValue = value;
+                this.EnableDisableElement();
+            }
         }
 
         protected override void Invoke(object parameter)
@@ -23,30 +90,104 @@
                 return;
             }
 
-            var command = this.Command;
+            ICommand command = this.Command;
             var commandParameter = this.CommandParameterValue;
 
-            PopulatingEventArgs e = (PopulatingEventArgs)parameter;
-            AutoCompleteBox box = (AutoCompleteBox)this.AssociatedObject;
+            PopulatingCompletedToken token = new PopulatingCompletedToken(this.AssociatedObject, commandParameter, (PopulatingEventArgs)parameter);
 
-            var cp = new AutoCompleteParameter
-                {
-                    CancelPopulating = () => e.Cancel = true,
-                    PopulateComplete = box.PopulateComplete,
-                    CommandParameter = commandParameter
-                };
-
-            if (command != null && command.CanExecute(cp))
+            if (command != null && command.CanExecute(token))
             {
-                command.Execute(cp);
+                command.Execute(token);
             }
+        }
+
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            this.EnableDisableElement();
+        }
+
+        private static void OnCommandParameterChanged(DependencyObject s, DependencyPropertyChangedEventArgs e)
+        {
+            var sender = s as AutoCompleteToCommand;
+            if (sender == null)
+            {
+                return;
+            }
+
+            if (sender.AssociatedObject == null)
+            {
+                return;
+            }
+
+            sender.EnableDisableElement();
+        }
+
+        private static void OnCommandChanged(AutoCompleteToCommand element, DependencyPropertyChangedEventArgs e)
+        {
+            if (element == null)
+            {
+                return;
+            }
+
+            if (e.OldValue != null)
+            {
+                ((ICommand)e.OldValue).CanExecuteChanged -= element.OnCommandCanExecuteChanged;
+            }
+
+            var command = (ICommand)e.NewValue;
+
+            if (command != null)
+            {
+                command.CanExecuteChanged += element.OnCommandCanExecuteChanged;
+            }
+
+            element.EnableDisableElement();
+        }
+
+        private static void OnMustToggleIsEnabledChanged(DependencyObject s, DependencyPropertyChangedEventArgs e)
+        {
+            var sender = s as AutoCompleteToCommand;
+            if (sender == null)
+            {
+                return;
+            }
+
+            if (sender.AssociatedObject == null)
+            {
+                return;
+            }
+
+            sender.EnableDisableElement();
         }
 
         private bool AssociatedElementIsDisabled()
         {
-            Control element = this.AssociatedObject as Control;
+            var element = this.AssociatedObject;
 
             return this.AssociatedObject == null || (element != null && !element.IsEnabled);
+        }
+
+        private void OnCommandCanExecuteChanged(object sender, EventArgs e)
+        {
+            this.EnableDisableElement();
+        }
+
+        private void EnableDisableElement()
+        {
+            var element = this.AssociatedObject;
+
+            if (element == null)
+            {
+                return;
+            }
+
+            var command = this.Command;
+
+            if (this.MustToggleIsEnabledValue && command != null)
+            {
+                element.IsEnabled = command.CanExecute(new PopulatingCompletedToken(this.AssociatedObject, this.CommandParameterValue, null));
+            }
         }
     }
 }
