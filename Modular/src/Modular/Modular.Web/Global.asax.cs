128cf68695e68c635d44ada0140eca82a42c8fe7
weberse <weberse@localhost>
2014-02-04 16:58:10 +0100

    [FEATURE] added signalr for sending notifications to the clients.

---
+++ Modular/src/Modular/Modular.Web/Global.asax.cs
@@ -0,0 +1,52 @@
+﻿using System.Web.Routing;
+using Microsoft.AspNet.SignalR;
+
+namespace Modular.Web
+{
+    using System;
+
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            HubConfiguration configuration = new HubConfiguration
+                {
+                    EnableCrossDomain = true,
+                    EnableDetailedErrors = true,
+                    EnableJavaScriptProxies = true
+                };
+
+            RouteTable.Routes.MapHubs(configuration);
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
