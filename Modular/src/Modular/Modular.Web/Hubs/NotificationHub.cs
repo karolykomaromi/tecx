d57e87b0169d18bec94d482e091720dada4090cc
weberse <weberse@localhost>
2014-02-04 16:58:10 +0100

    [FEATURE] added signalr for sending notifications to the clients.

---
+++ Modular/src/Modular/Modular.Web/Hubs/NotificationHub.cs
@@ -0,0 +1,12 @@
+﻿namespace Modular.Web.Hubs
+{
+    using Microsoft.AspNet.SignalR;
+
+    public class NotificationHub : Hub
+    {
+        public void Notify(string notification)
+        {
+            this.Clients.All.notify(notification);
+        }
+    }
+}
