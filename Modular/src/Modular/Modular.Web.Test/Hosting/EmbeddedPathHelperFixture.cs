26a9b6810bff869de2c01482e378b85f734f585b
snd\Damien80_cp <snd\Damien80_cp@localhost>
2014-02-26 23:21:11 +0100

    [FEATURE] playing with virtual path provider that maps to embedded resources

---
+++ Modular/src/Modular/Modular.Web.Test/Hosting/EmbeddedPathHelperFixture.cs
@@ -0,0 +1,34 @@
+﻿namespace Modular.Web.Test.Hosting
+{
+    using System;
+    using System.Collections.Generic;
+    using Modular.Web.Hosting;
+    using Xunit;
+
+    public class EmbeddedPathHelperFixture
+    {
+        [Fact]
+        public void Should_Transform_ManifestResourceName_To_App_Relative_Path()
+        {
+            string manifestResourceName = "Modular.Web.Test.Assets.Texts.Long.VeryLong.txt";
+
+            string expected = "~/Texts/Long/VeryLong.txt";
+
+            string actual = EmbeddedPathHelper.ToAppRelative(manifestResourceName);
+
+            Assert.Equal(expected, actual, StringComparer.OrdinalIgnoreCase);
+        }
+
+        [Fact]
+        public void Should_Get_AppRelative_Directories_From_ManifestResourceName()
+        {
+            string manifestResourceName = "Modular.Web.Test.Assets.Texts.Long.VeryLong.txt";
+
+            IEnumerable<string> expected = new[] { "~/Texts/", "~/Texts/Long/" };
+
+            IEnumerable<string> actual = EmbeddedPathHelper.GetDirectories(manifestResourceName);
+
+            Assert.Equal(expected, actual, StringComparer.OrdinalIgnoreCase);
+        }
+    }
+}
---
+++ Modular/src/Modular/Modular.Web/Hosting/EmbeddedResourceVirtualPathProvider.cs
@@ -0,0 +1,71 @@
+﻿namespace Modular.Web.Hosting
+{
+    using System;
+    using System.Collections;
+    using System.Diagnostics.Contracts;
+    using System.Linq;
+    using System.Reflection;
+    using System.Web.Caching;
+    using System.Web.Hosting;
+
+    public class EmbeddedResourceVirtualPathProvider : VirtualPathProvider
+    {
+        private readonly Assembly assembly;
+        private readonly IVirtualPathUtility virtualPathUtility;
+
+        public EmbeddedResourceVirtualPathProvider(Assembly assembly, IVirtualPathUtility virtualPathUtility)
+        {
+            Contract.Requires(assembly != null);
+            Contract.Requires(virtualPathUtility != null);
+
+            this.assembly = assembly;
+            this.virtualPathUtility = virtualPathUtility;
+        }
+
+        public override bool FileExists(string virtualPath)
+        {
+            return base.FileExists(virtualPath);
+        }
+
+        public override VirtualFile GetFile(string virtualPath)
+        {
+            if (this.IsEmbeddedResource(virtualPath))
+            {
+                return new EmbeddedResourceVirtualFile(virtualPath, this.assembly, this.virtualPathUtility);
+            }
+
+            return this.Previous.GetFile(virtualPath);
+        }
+
+        public override bool DirectoryExists(string virtualDir)
+        {
+            return base.DirectoryExists(virtualDir);
+        }
+
+        public override VirtualDirectory GetDirectory(string virtualDir)
+        {
+            if (this.IsEmbeddedResource(virtualDir))
+            {
+                return new EmbeddedResourceVirtualDirectory(virtualDir, this.assembly, this.virtualPathUtility);
+            }
+
+            return this.Previous.GetDirectory(virtualDir);
+        }
+
+        public override CacheDependency GetCacheDependency(string virtualPath, IEnumerable virtualPathDependencies, DateTime utcStart)
+        {
+            if (this.IsEmbeddedResource(virtualPath))
+            {
+                // TODO weberse 2014-02-26 need to figure out how to create a meaningful cache dependency
+                return null;
+            }
+
+            return this.Previous.GetCacheDependency(virtualPath, virtualPathDependencies, utcStart);
+        }
+
+        public bool IsEmbeddedResource(string virtualPath)
+        {
+            return false;
+        }
+    }
+}
