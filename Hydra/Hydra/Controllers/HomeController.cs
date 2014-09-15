321386e1de2ecf3748025a3cb534839bac5e2395
weberse <weberse@localhost>
2014-09-15 15:51:53 +0200

    [UPDATE] added stylecop

--- Hydra/Hydra/Controllers/HomeController.cs
+++ Hydra/Hydra/Controllers/HomeController.cs
@@ -1,18 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-
-namespace Hydra.Controllers
+﻿namespace Hydra.Controllers
 {
+    using System.Web.Mvc;
+
     public class HomeController : Controller
     {
-        //
         // GET: /Home/
         public ActionResult Index()
         {
-            return Content("Hello World!");
+            return this.Content("Hello World!");
         }
-	}
+    }
 }
