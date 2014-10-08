2d27dfe6150513520feb74f7dd87bdb6ab572918
weberse <weberse@localhost>
2014-10-08 15:57:06 +0200

    [FEATURE] always wanted to play around with entlib semantic logging since it was first announced

---
+++ Hydra/Hydra.Infrastructure/Logging/DebugLog.cs
@@ -0,0 +1,20 @@
+﻿namespace Hydra.Infrastructure.Logging
+{
+    using System;
+    using Microsoft.Practices.EnterpriseLibrary.SemanticLogging;
+    using Microsoft.Practices.EnterpriseLibrary.SemanticLogging.Formatters;
+
+    public static class DebugLog
+    {
+        public static SinkSubscription<DebugSink> LogToDebug(this IObservable<EventEntry> eventStream, IEventTextFormatter formatter = null)
+        {
+            formatter = formatter ?? new EventTextFormatter();
+
+            var sink = new DebugSink(formatter);
+
+            IDisposable subscription = eventStream.Subscribe(sink);
+
+            return new SinkSubscription<DebugSink>(subscription, sink);
+        }
+    }
+}
---
+++ Hydra/Hydra.Infrastructure/Logging/HydraEventSourceExtensions.cs
@@ -0,0 +1,33 @@
+﻿namespace Hydra.Infrastructure.Logging
+{
+    using System;
+    using System.Diagnostics.Contracts;
+
+    public static class HydraEventSourceExtensions
+    {
+        public static void Error(this HydraEventSource log, Exception exception)
+        {
+            Contract.Requires(log != null);
+            Contract.Requires(exception != null);
+
+            log.Error(exception.ToString());
+        }
+
+        public static void Critical(this HydraEventSource log, Exception exception)
+        {
+            Contract.Requires(log != null);
+            Contract.Requires(exception != null);
+
+            log.Critical(exception.ToString());
+        }
+
+        public static void MissingMapping(this HydraEventSource log, Type from, Type to)
+        {
+            Contract.Requires(log != null);
+            Contract.Requires(from != null);
+            Contract.Requires(to != null);
+
+            log.MissingMapping(from.AssemblyQualifiedName, to.AssemblyQualifiedName);
+        }
+    }
+}
