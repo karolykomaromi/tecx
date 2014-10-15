0b7b87db5d63914ca5e72fdf00c896020443d7cd
snd\Damien80_cp <snd\Damien80_cp@localhost>
2014-10-15 19:52:02 +0200

    [CLEAN] fixed stylecop warnings [UPDATE] builder base class, literal extensions

---
+++ Hydra/Hydra.Infrastructure/TimespanExtensions.cs
@@ -0,0 +1,57 @@
+﻿namespace Hydra.Infrastructure
+{
+    using System;
+
+    public static class TimeSpanExtensions
+    {
+        public static TimeSpan Milliseconds(this int milliseconds)
+        {
+            return TimeSpan.FromMilliseconds(milliseconds);
+        }
+
+        public static TimeSpan Seconds(this int seconds)
+        {
+            return TimeSpan.FromSeconds(seconds);
+        }
+
+        public static TimeSpan Minutes(this int minutes)
+        {
+            return TimeSpan.FromMinutes(minutes);
+        }
+
+        public static TimeSpan Hours(this int hours)
+        {
+            return TimeSpan.FromHours(hours);
+        }
+
+        public static TimeSpan Days(this int days)
+        {
+            return TimeSpan.FromDays(days);
+        }
+
+        public static TimeSpan Milliseconds(this double milliseconds)
+        {
+            return TimeSpan.FromMilliseconds(milliseconds);
+        }
+
+        public static TimeSpan Seconds(this double seconds)
+        {
+            return TimeSpan.FromSeconds(seconds);
+        }
+
+        public static TimeSpan Minutes(this double minutes)
+        {
+            return TimeSpan.FromMinutes(minutes);
+        }
+
+        public static TimeSpan Hours(this double hours)
+        {
+            return TimeSpan.FromHours(hours);
+        }
+
+        public static TimeSpan Days(this double days)
+        {
+            return TimeSpan.FromDays(days);
+        }
+    }
+}
