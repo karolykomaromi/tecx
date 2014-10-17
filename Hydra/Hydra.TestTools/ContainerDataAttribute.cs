834be801f258279a715a258bb21b987432603a54
weberse <weberse@localhost>
2014-10-17 16:10:09 +0200

    [CLEAN] moving stuff around

---
+++ Hydra/Hydra.TestTools/ContainerDataAttribute.cs
@@ -0,0 +1,16 @@
+﻿namespace Hydra.TestTools
+{
+    using Microsoft.Practices.Unity;
+    using Ploeh.AutoFixture;
+    using Ploeh.AutoFixture.Xunit;
+
+    public class ContainerDataAttribute : AutoDataAttribute
+    {
+        public ContainerDataAttribute()
+            : base(new Fixture().Customize(
+                new ContainerCustomization(
+                    new UnityContainer())))
+        {
+        }
+    }
+}
---
+++ Hydra/Hydra.TestTools/MailAddressBuilderTests.cs
@@ -0,0 +1,17 @@
+﻿namespace Hydra.TestTools
+{
+    using System.Net.Mail;
+    using Xunit;
+    using Xunit.Extensions;
+
+    public class MailAddressBuilderTests
+    {
+        [Theory, ContainerData]
+        public void Should_Build_Address_With_Two_Part_Last_Name(MailAddressBuilder sut)
+        {
+            MailAddress actual = sut.VictorSenYung();
+
+            Assert.Equal("victor.yung@mail.invalid", actual.Address);
+        }
+    }
+}
