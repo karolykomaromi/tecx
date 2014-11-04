c09474f2bfb5506a6b0d13920bfd50fd039b49d7
weberse <weberse@localhost>
2014-11-04 13:11:33 +0100

    [UPDATE] working on mappings for automapper

---
+++ Hydra/Hydra.Jobs.Test/ContainerDataAttribute.cs
@@ -0,0 +1,25 @@
+﻿namespace Hydra.Jobs.Test
+{
+    using System;
+    using AutoMapper;
+    using Hydra.Jobs.Transfer;
+    using Hydra.TestTools;
+    using Microsoft.Practices.Unity;
+    using Ploeh.AutoFixture;
+    using Ploeh.AutoFixture.Xunit;
+
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
+    public class ContainerDataAttribute : AutoDataAttribute
+    {
+        public ContainerDataAttribute()
+            : base(new Fixture().Customize(
+                new ContainerCustomization(
+                    new UnityContainer().RegisterType<IMappingEngine>(new InjectionFactory(_ =>
+                        {
+                            Mapper.AddProfile<JobsProfile>();
+                            return Mapper.Engine;
+                        })))))
+        {
+        }
+    }
+}
