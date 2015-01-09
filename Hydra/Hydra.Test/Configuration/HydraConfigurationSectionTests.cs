06e4d6d6b43d3c3923816c611a7f213a2350f553
weberse <weberse@localhost>
2015-01-09 09:45:28 +0100

    [CLEAN] put config stuff in appropriate folder

--- Hydra/Hydra.Test/Configuration/HydraConfigurationSectionTests.cs
+++ Hydra/Hydra.Test/Configuration/HydraConfigurationSectionTests.cs
@@ -1,9 +1,8 @@
 ﻿namespace Hydra.Test.Configuration
 {
-    using System.ComponentModel;
     using System.Configuration;
-    using System.Globalization;
     using System.Linq;
+    using Hydra.Configuration;
     using Hydra.Infrastructure.I18n;
     using Xunit;
 
@@ -17,74 +16,9 @@
             Assert.NotNull(group);
             Assert.NotNull(group.ApplicationSettings);
             Assert.NotNull(group.ApplicationSettings.SupportedLanguages);
-            Assert.Equal(1, group.ApplicationSettings.SupportedLanguages.Count);
-            Assert.Equal(Cultures.GermanGermany, group.ApplicationSettings.SupportedLanguages.OfType<SupportedLanguage>().First().Culture);
-        }
-    }
-
-    public class HydraConfigurationSectionGroup : ConfigurationSectionGroup
-    {
-        [ConfigurationProperty("applicationSettings", IsRequired = true)]
-        public HydraApplicationSettings ApplicationSettings
-        {
-            get { return (HydraApplicationSettings)base.Sections["applicationSettings"]; }
-        }
-    }
-
-    public class HydraApplicationSettings : ConfigurationSection
-    {
-        [ConfigurationProperty("supportedLanguages", IsRequired = true)]
-        public SupportedLanguagesCollection SupportedLanguages
-        {
-            get { return (SupportedLanguagesCollection)base["supportedLanguages"]; }
-        }
-    }
-
-    public class SupportedLanguagesCollection : ConfigurationElementCollection
-    {
-        protected override ConfigurationElement CreateNewElement()
-        {
-            return new SupportedLanguage();
-        }
-
-        protected override object GetElementKey(ConfigurationElement element)
-        {
-            return ((SupportedLanguage)element).Culture.ToString();
-        }
-    }
-
-    public class SupportedLanguage : ConfigurationElement
-    {
-        private static readonly ConfigurationProperty languageProperty;
-
-        private static readonly ConfigurationPropertyCollection properties;
-
-        static SupportedLanguage()
-        {
-            languageProperty = new ConfigurationProperty(
-                "lang",
-                typeof (CultureInfo),
-                CultureInfo.InvariantCulture,
-                new CultureInfoConverter(),
-                null,
-                ConfigurationPropertyOptions.None);
-
-            properties = new ConfigurationPropertyCollection
-                {
-                    languageProperty
-                };
-        }
-
-        public CultureInfo Culture
-        {
-            get { return (CultureInfo) base[languageProperty]; }
-
-            set { base[languageProperty] = value; }
-        }
-
-        protected override ConfigurationPropertyCollection Properties
-        {
-            get { return properties; }
+            Assert.Equal(2, group.ApplicationSettings.SupportedLanguages.Count);
+            Assert.Equal(Cultures.GermanNeutral, group.ApplicationSettings.SupportedLanguages.OfType<SupportedLanguage>().First().Culture);
+            Assert.Equal(Cultures.GermanGermany, group.ApplicationSettings.SupportedLanguages.OfType<SupportedLanguage>().ElementAt(1).Culture);
         }
     }
 }
---
+++ Hydra/Hydra/Configuration/SupportedLanguage.cs
@@ -0,0 +1,41 @@
+namespace Hydra.Configuration
+{
+    using System.ComponentModel;
+    using System.Configuration;
+    using System.Globalization;
+
+    public class SupportedLanguage : ConfigurationElement
+    {
+        private static readonly ConfigurationProperty LanguageProperty;
+
+        private static readonly ConfigurationPropertyCollection ConfigProperties;
+
+        static SupportedLanguage()
+        {
+            LanguageProperty = new ConfigurationProperty(
+                "lang",
+                typeof(CultureInfo),
+                CultureInfo.InvariantCulture,
+                new CultureInfoConverter(),
+                null,
+                ConfigurationPropertyOptions.None);
+
+            ConfigProperties = new ConfigurationPropertyCollection
+            {
+                LanguageProperty
+            };
+        }
+
+        public CultureInfo Culture
+        {
+            get { return (CultureInfo)base[LanguageProperty]; }
+
+            set { base[LanguageProperty] = value; }
+        }
+
+        protected override ConfigurationPropertyCollection Properties
+        {
+            get { return ConfigProperties; }
+        }
+    }
+}
