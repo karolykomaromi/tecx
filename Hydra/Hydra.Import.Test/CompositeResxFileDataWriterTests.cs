457a5b2342b2123700ccff99081093744f758dc9
weberse <weberse@localhost>
2015-01-16 14:22:28 +0100

    [UPDATE] and forgot to save again...

--- Hydra/Hydra.Import.Test/CompositeResxFileDataWriterTests.cs
+++ Hydra/Hydra.Import.Test/CompositeResxFileDataWriterTests.cs
@@ -1,13 +1,7 @@
 ﻿namespace Hydra.Import.Test
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Diagnostics.Contracts;
-    using System.Globalization;
-    using System.IO;
     using System.Resources;
     using Hydra.Infrastructure.I18n;
-    using Hydra.Infrastructure.Logging;
     using Xunit;
 
     public class CompositeResxFileDataWriterTests
@@ -31,83 +25,6 @@
             Assert.Equal("1", resourceManager.GetString("Foo", Cultures.GermanNeutral));
 
             Assert.Equal("2", resourceManager.GetString("Foo", Cultures.EnglishNeutral));
-        }
-    }
-
-    public class CompositeResxFileDataWriter : IDataWriter<ResourceItem>, IDisposable
-    {
-        private readonly string baseName;
-
-        private readonly string directoryForResxFiles;
-
-        private readonly HashSet<Stream> streams;
-
-        private readonly IDictionary<CultureInfo, ResxFileDataWriter> targetFiles;
-
-        public CompositeResxFileDataWriter(string baseName, string directoryForResxFiles)
-        {
-            Contract.Requires(!string.IsNullOrWhiteSpace(baseName));
-            Contract.Requires(!string.IsNullOrWhiteSpace(directoryForResxFiles));
-            Contract.Requires(Directory.Exists(directoryForResxFiles));
-
-            this.baseName = baseName.EndsWith(".", StringComparison.Ordinal) ? baseName : baseName + ".";
-            this.directoryForResxFiles = directoryForResxFiles;
-            this.targetFiles = new Dictionary<CultureInfo, ResxFileDataWriter>();
-            this.streams = new HashSet<Stream>();
-        }
-
-        public ImportResult Write(IEnumerable<ResourceItem> items)
-        {
-            foreach (ResourceItem ri in items)
-            {
-                if (ri.Language == null)
-                {
-                    continue;
-                }
-
-                ResxFileDataWriter writer;
-                if (!this.targetFiles.TryGetValue(ri.Language, out writer))
-                {
-                    string resxFilePath = Path.Combine(this.directoryForResxFiles, this.baseName + ri.Language.Name + ".resources");
-
-                    Stream stream = new FileStream(resxFilePath, FileMode.Create);
-
-                    writer = new ResxFileDataWriter(stream);
-
-                    this.targetFiles.Add(ri.Language, writer);
-
-                    this.streams.Add(stream);
-                }
-
-                writer.Write(new[] { ri });
-            }
-
-            return new ImportFailed();
-        }
-
-        public void Dispose()
-        {
-            this.Dispose(true);
-
-            GC.SuppressFinalize(this);
-        }
-
-        private void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                foreach (Stream stream in this.streams)
-                {
-                    try
-                    {
-                        stream.Dispose();
-                    }
-                    catch (Exception ex)
-                    {
-                        HydraEventSource.Log.Error(ex);
-                    }
-                }
-            }
         }
     }
 }
