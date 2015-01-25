c13861ceea90f29f7c6656b8cc7716e6f1bb7533
snd\Damien80_cp <snd\Damien80_cp@localhost>
2015-01-25 12:10:36 +0100

    [FEATURE] composite settings provider will be base for app settings mgmt. default in memory first, then db or config or both, wrapped in cache

--- Hydra/Hydra.Infrastructure.Test/Configuration/NhSettingsProviderTests.cs
+++ Hydra/Hydra.Infrastructure.Test/Configuration/NhSettingsProviderTests.cs
@@ -1,6 +1,14 @@
 ﻿namespace Hydra.Infrastructure.Test.Configuration
 {
+    using System;
+    using Xunit;
+
     public class NhSettingsProviderTests
     {
+        [Fact]
+        public void Should_Load_Settings_From_Session()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
