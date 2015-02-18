305f4fd864a2964980f335c1004ef13fd097ddb2
weberse <weberse@localhost>
2015-02-18 17:10:56 +0100

    [FEATURE] starting on send mail job

---
+++ Hydra/Hydra.Jobs.Test/Jobs/SendMailTests.cs
@@ -0,0 +1,48 @@
+﻿namespace Hydra.Jobs.Test.Jobs
+{
+    using System.Net.Mail;
+    using Hydra.Infrastructure;
+    using Hydra.Infrastructure.Calendaring;
+    using Hydra.TestTools;
+    using netDumbster.smtp;
+    using Xunit;
+
+    public class SendMailTests
+    {
+        [Fact]
+        public async void Should_Send_Mail()
+        {
+            SimpleSmtpServer server = null;
+
+            try
+            {
+                server = SimpleSmtpServer.Start();
+
+                using (var client = new SmtpClient("localhost", server.Port))
+                {
+                    string johnWayne = new MailAddressBuilder().JohnWayne().Build().ToString();
+                    string clintEastwood = new MailAddressBuilder().ClintEastwood().Build().ToString();
+
+                    await client.SendMailAsync(johnWayne, clintEastwood, "Foo!", "Bar");
+                }
+
+                Assert.Equal(1, server.ReceivedEmailCount);
+            }
+            finally
+            {
+                if (server != null)
+                {
+                    server.Stop();
+                }
+            }
+        }
+    }
+
+    public class MailMessageBuilder : Builder<MailMessage>
+    {
+        public override MailMessage Build()
+        {
+            return new MailMessage();
+        }
+    }
+}
