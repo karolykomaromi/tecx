f453d1d4b0b6199e35513b96330f25127983feeb
weberse <weberse@localhost>
2015-02-19 18:49:23 +0100

    [UPDATE] working on mail message builder, test for basic sendmail job

--- Hydra/Hydra.Infrastructure/Mail/MailMessageBuilder.cs
+++ Hydra/Hydra.Infrastructure/Mail/MailMessageBuilder.cs
@@ -1,12 +1,234 @@
 namespace Hydra.Infrastructure.Mail
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.Specialized;
+    using System.Diagnostics.Contracts;
     using System.Net.Mail;
+    using System.Text;
 
     public class MailMessageBuilder : Builder<MailMessage>
     {
+        private readonly HashSet<MailAddress> replyTo;
+        private readonly HashSet<MailAddress> recipients;
+        private readonly HashSet<MailAddress> cc;
+        private readonly HashSet<MailAddress> bcc;
+        private readonly HashSet<Attachment> attachments;
+        private readonly NameValueCollection headers;
+        private MailAddress sender;
+        private string subject;
+        private string body;
+        private Encoding bodyEncoding;
+        private Encoding headerEncoding;
+        private bool isBodyHtml;
+
+        public MailMessageBuilder()
+        {
+            this.replyTo = new HashSet<MailAddress>();
+            this.recipients = new HashSet<MailAddress>();
+            this.cc = new HashSet<MailAddress>();
+            this.bcc = new HashSet<MailAddress>();
+            this.attachments = new HashSet<Attachment>();
+            this.headers = new NameValueCollection();
+            this.subject = string.Empty;
+            this.body = string.Empty;
+            this.isBodyHtml = false;
+        }
+
         public override MailMessage Build()
         {
-            return new MailMessage();
+            Contract.Requires(this.sender != null);
+            Contract.Requires(this.recipients.Count > 0);
+
+            MailMessage message = new MailMessage
+            {
+                From = this.sender,
+                Subject = this.subject,
+                Body = this.body,
+                IsBodyHtml = this.isBodyHtml
+            };
+
+            if (this.bodyEncoding != null)
+            {
+                message.BodyEncoding = this.bodyEncoding;
+            }
+
+            if (this.headerEncoding != null)
+            {
+                message.HeadersEncoding = this.bodyEncoding;
+            }
+
+            foreach (MailAddress recipient in this.replyTo)
+            {
+                message.ReplyToList.Add(recipient);
+            }
+
+            foreach (MailAddress recipient in this.recipients)
+            {
+                message.To.Add(recipient);
+            }
+
+            foreach (MailAddress recipient in this.cc)
+            {
+                message.CC.Add(recipient);
+            }
+
+            foreach (MailAddress recipient in this.bcc)
+            {
+                message.Bcc.Add(recipient);
+            }
+
+            foreach (Attachment attachment in this.attachments)
+            {
+                message.Attachments.Add(attachment);
+            }
+
+            for (int i = 0; i < this.headers.Count; i++)
+            {
+                message.Headers.Add(this.headers.GetKey(i), this.headers.Get(i));
+            }
+
+            return message;
+        }
+
+        public MailMessageBuilder From(Action<MailAddressBuilder> action)
+        {
+            Contract.Requires(action != null);
+            Contract.Ensures(Contract.Result<MailMessageBuilder>() != null);
+
+            MailAddressBuilder builder = new MailAddressBuilder();
+
+            action(builder);
+
+            this.sender = builder;
+
+            return this;
+        }
+
+        public MailMessageBuilder ReplyTo(Action<MailAddressBuilder> action)
+        {
+            Contract.Requires(action != null);
+            Contract.Ensures(Contract.Result<MailMessageBuilder>() != null);
+
+            MailAddressBuilder builder = new MailAddressBuilder();
+
+            action(builder);
+
+            this.replyTo.Add(builder);
+
+            return this;
+        }
+
+        public MailMessageBuilder WithRecipient(Action<MailAddressBuilder> action)
+        {
+            Contract.Requires(action != null);
+            Contract.Ensures(Contract.Result<MailMessageBuilder>() != null);
+
+            MailAddressBuilder builder = new MailAddressBuilder();
+
+            action(builder);
+
+            this.recipients.Add(builder);
+
+            return this;
+        }
+
+        public MailMessageBuilder WithCarbonCopy(Action<MailAddressBuilder> action)
+        {
+            Contract.Requires(action != null);
+            Contract.Ensures(Contract.Result<MailMessageBuilder>() != null);
+
+            MailAddressBuilder builder = new MailAddressBuilder();
+
+            action(builder);
+
+            this.cc.Add(builder);
+
+            return this;
+        }
+
+        public MailMessageBuilder WithBlindCarbonCopy(Action<MailAddressBuilder> action)
+        {
+            Contract.Requires(action != null);
+            Contract.Ensures(Contract.Result<MailMessageBuilder>() != null);
+
+            MailAddressBuilder builder = new MailAddressBuilder();
+
+            action(builder);
+
+            this.bcc.Add(builder);
+
+            return this;
+        }
+
+        public MailMessageBuilder WithSubject(string subject)
+        {
+            Contract.Requires(subject != null);
+            Contract.Ensures(Contract.Result<MailMessageBuilder>() != null);
+
+            this.subject = subject;
+
+            return this;
+        }
+
+        public MailMessageBuilder WithHeader(string headerName, string headerValue)
+        {
+            Contract.Requires(headerName != null);
+            Contract.Requires(headerValue != null);
+            Contract.Ensures(Contract.Result<MailMessageBuilder>() != null);
+
+            this.headers.Add(headerName, headerValue);
+
+            return this;
+        }
+
+        public MailMessageBuilder WithBody(string body)
+        {
+            Contract.Requires(body != null);
+            Contract.Ensures(Contract.Result<MailMessageBuilder>() != null);
+
+            this.body = body;
+
+            return this;
+        }
+
+        public MailMessageBuilder WithAttachment(Attachment attachment)
+        {
+            Contract.Requires(attachment != null);
+            Contract.Ensures(Contract.Result<MailMessageBuilder>() != null);
+
+            this.attachments.Add(attachment);
+
+            return this;
+        }
+        
+        public MailMessageBuilder WithBodyEncoding(Encoding encoding)
+        {
+            Contract.Requires(encoding != null);
+            Contract.Ensures(Contract.Result<MailMessageBuilder>() != null);
+
+            this.bodyEncoding = encoding;
+
+            return this;
+        }
+        
+        public MailMessageBuilder WithHeaderEncoding(Encoding encoding)
+        {
+            Contract.Requires(encoding != null);
+            Contract.Ensures(Contract.Result<MailMessageBuilder>() != null);
+
+            this.headerEncoding = encoding;
+
+            return this;
+        }
+        
+        public MailMessageBuilder IsHtml()
+        {
+            Contract.Ensures(Contract.Result<MailMessageBuilder>() != null);
+
+            this.isBodyHtml = true;
+
+            return this;
         }
     }
 }
--- Hydra/Hydra.Jobs.Server/Jobs/SendMail.cs
+++ Hydra/Hydra.Jobs.Server/Jobs/SendMail.cs
@@ -1,11 +1,92 @@
 ﻿namespace Hydra.Jobs.Server.Jobs
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Net;
+    using System.Net.Mail;
+    using Hydra.Infrastructure.Logging;
     using Quartz;
 
     public class SendMail : IJob
     {
+        private readonly IUnsentMailQueue unsent;
+        private readonly ISentMailQueue sent;
+        private readonly MailSettings mailSettings;
+
+        public SendMail(IUnsentMailQueue unsent, ISentMailQueue sent, MailSettings mailSettings)
+        {
+            this.unsent = unsent;
+            this.sent = sent;
+            this.mailSettings = mailSettings;
+        }
+
         public void Execute(IJobExecutionContext context)
         {
+            using (var client = new SmtpClient(this.mailSettings.Host, this.mailSettings.Port) { Credentials = this.mailSettings.Credentials })
+            {
+                MailMessage message;
+                while ((message = this.unsent.Dequeue()) != null)
+                {
+                    try
+                    {
+                        client.Send(message);
+
+                        this.sent.Enqueue(message);
+                    }
+                    catch (Exception ex)
+                    {
+                        HydraEventSource.Log.Error(ex);
+                    }
+                }
+            }
+        }
+    }
+
+    public class MailSettings
+    {
+        public virtual ICredentialsByHost Credentials { get; set; }
+
+        public virtual int Port { get; set; }
+
+        public virtual string Host { get; set; }
+    }
+
+    public interface IUnsentMailQueue
+    {
+        void Enqueue(MailMessage message);
+
+        MailMessage Dequeue();
+    }
+
+    public interface ISentMailQueue
+    {
+        void Enqueue(MailMessage message);
+
+        MailMessage Dequeue();
+    }
+
+    public class InMemoryMailQueue : IUnsentMailQueue, ISentMailQueue
+    {
+        private readonly Queue<MailMessage> messages;
+
+        public InMemoryMailQueue(params MailMessage[] messages)
+        {
+            this.messages = new Queue<MailMessage>(messages ?? new MailMessage[0]);
+        }
+
+        public void Enqueue(MailMessage message)
+        {
+            this.messages.Enqueue(message);
+        }
+
+        public MailMessage Dequeue()
+        {
+            if (this.messages.Count > 0)
+            {
+                return this.messages.Dequeue();
+            }
+
+            return null;
         }
     }
 }
