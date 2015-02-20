72968809ea8bf37516f1759eaca53c6da19fe3fd
weberse <weberse@localhost>
2015-02-20 14:26:49 +0100

    [FIX] t4 vs and msbuild host treat project properties in assembly directives differently

---
+++ Hydra/Hydra.Infrastructure.Test/Mail/EmbeddedResourceMailSourceTests.cs
@@ -0,0 +1,23 @@
+﻿namespace Hydra.Infrastructure.Test.Mail
+{
+    using System.Linq;
+    using System.Net.Mail;
+    using Hydra.Infrastructure.Mail;
+    using Hydra.TestTools;
+    using Xunit;
+
+    public class EmbeddedResourceMailSourceTests
+    {
+        [Fact]
+        public void Should_Return_MailMessage_From_EmbeddedResource()
+        {
+            IUnsentMailSource sut = new EmbeddedResourceMailSource(this.GetType().Assembly, KnownTestFiles.TestFiles.Mails.Mail01);
+
+            MailMessage actual = sut.Single();
+
+            MailAddress from = new MailAddressBuilder().ClintEastwood();
+
+            Assert.Equal(from, actual.From);
+        }
+    }
+}
