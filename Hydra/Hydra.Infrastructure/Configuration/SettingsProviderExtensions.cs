b77cc5c223cd94a396ec923ff241265b4e737ff1
snd\Damien80_cp <snd\Damien80_cp@localhost>
2015-02-22 14:14:50 +0100

    [FEATURE] finally got to working on sensible default values

---
+++ Hydra/Hydra.Infrastructure/Configuration/SettingsProviderExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace Hydra.Infrastructure.Configuration
+{
+    using System.Diagnostics.Contracts;
+
+    public static class SettingsProviderExtensions
+    {
+        public static T GetValue<T>(this ISettingsProvider provider, SettingName settingName)
+        {
+            Contract.Requires(provider != null);
+            Contract.Requires(settingName != null);
+
+            object value = provider.GetSettings()[settingName];
+
+            if (value is T)
+            {
+                return (T)value;
+            }
+
+            return default(T);
+        }
+    }
+}
