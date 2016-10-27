06e881ebfa2a1b3f88bde743589d5b1e38f47138
weberse <weberse@localhost>
2016-10-27 09:48:41 +0200

    [CLEAN] minor updates

---
+++ Janus/Janus.I18n/Tools/Install.ps1
@@ -0,0 +1,13 @@
+﻿param($installPath, $toolsPath, $package, $project)
+
+$buildProject = @([Microsoft.Build.Evaluation.ProjectCollection]::GlobalProjectCollection.GetLoadedProjects($project.FullName))[0]  
+
+$file = $buildProject.Xml.Items | Where Include -eq "Resources.tt"
+$propertyToEdit = $file.Metadata | Where Name -eq "DependentUpon"
+
+if (!$propertyToEdit)
+{
+    $file.AddMetaData("DependentUpon", "Resources.resx") | Out-Null
+}
+
+$project.Save()
